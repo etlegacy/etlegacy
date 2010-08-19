574789e710b22f9cbc299502f186bff0ecbc08fb
Ralph Eastwood <tcmreastwood@gmail.com>
2010-08-19 20:54:02 +0100

    Win32 compile fixes.

--- src/sys/sys_win32.c
+++ src/sys/sys_win32.c
@@ -26,7 +26,7 @@ If you have questions concerning this license or the applicable additional terms
 ===========================================================================
 */
 
-#include "../qcommon/q_shared.h"
+#include "../game/q_shared.h"
 #include "../qcommon/qcommon.h"
 #include "sys_local.h"
 
