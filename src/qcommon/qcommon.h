c42498eed6c00dcd8194ab66f97d31e5a3c45d59
IR4T4 <gutzu@gmx.ch>
2012-08-07 14:05:09 +0200

    clean up refs #31

--- src/qcommon/qcommon.h
+++ src/qcommon/qcommon.h
@@ -818,18 +818,6 @@ typedef struct gameInfo_s
 
 extern gameInfo_t com_gameInfo;
 
-// returnbed by Sys_GetProcessorId
-#define CPUID_GENERIC           0           // any unrecognized processor
-
-#define CPUID_AXP               0x10
-
-#define CPUID_INTEL_UNSUPPORTED 0x20            // Intel 386/486
-#define CPUID_INTEL_PENTIUM     0x21            // Intel Pentium or PPro
-#define CPUID_INTEL_MMX         0x22            // Intel Pentium/MMX or P2/MMX
-#define CPUID_INTEL_KATMAI      0x23            // Intel Katmai
-
-#define CPUID_AMD_3DNOW         0x30            // AMD K6 3DNOW!
-
 // returned by Sys_GetProcessorFeatures
 typedef enum
 {
