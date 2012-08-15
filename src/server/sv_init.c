8d65c9fbab86e8f1820747d48f95d7287e2ac1b4
IR4T4 <gutzu@gmx.ch>
2012-08-15 22:41:44 +0200

    sv_timeout default value changed to 40 refs #29

--- src/server/sv_init.c
+++ src/server/sv_init.c
@@ -830,7 +830,7 @@ void SV_Init(void)
 	sv_rconPassword    = Cvar_Get("rconPassword", "", CVAR_TEMP);
 	sv_privatePassword = Cvar_Get("sv_privatePassword", "", CVAR_TEMP);
 	sv_fps             = Cvar_Get("sv_fps", "20", CVAR_TEMP);
-	sv_timeout         = Cvar_Get("sv_timeout", "15", CVAR_TEMP);
+	sv_timeout         = Cvar_Get("sv_timeout", "40", CVAR_TEMP); // used in game (also vid_restart)
 	sv_dl_timeout      = Cvar_Get("sv_dl_timeout", "240", CVAR_TEMP);
 	sv_zombietime      = Cvar_Get("sv_zombietime", "2", CVAR_TEMP);
 	Cvar_Get("nextmap", "", CVAR_TEMP);
