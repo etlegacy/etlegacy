2b7f221f5d3ffc3710c8512fa717a20de4d6ebc0
IR4T4 <gutzu@gmx.ch>
2012-11-04 22:01:22 +0100

    game linux: adjustements for OB refs #86, refs #94

--- misc/etl_bot.sh
+++ misc/etl_bot.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # Simple script to start ET Legacy client/listen server with Omni-Bots
 #
-./etl +set omnibot_enable 1 +set omnibot_path "./legacy/omni-bot0.82"
+./etl +set omnibot_enable 1 +set omnibot_path "./legacy/omni-bot"
