5d89c6b12731a1f2f6f48b21fe3a193f16449ae1
IR4T4 <gutzu@gmx.ch>
2012-11-24 02:01:15 +0100

    generic: no color for info message

--- src/qcommon/files.c
+++ src/qcommon/files.c
@@ -4351,7 +4351,7 @@ void FS_InitFilesystem(void)
 		tmp_fs_game         = Cvar_Get("fs_game", "", 0);
 		tmp_fs_game->flags |= CVAR_USER_CREATED; // deal as startup var
 
-		Com_Printf("^2Info: fs_game is set to '%s' mod. Start ET:L with param '+set etmain' for adoring history.\n", tmp_fs_game->string);
+		Com_Printf("Info: fs_game is set to '%s' mod. Start ET:L with param '+set etmain' for adoring history.\n", tmp_fs_game->string);
 	}
 
 	Com_StartupVariable("fs_copyfiles");
