918b9885187c2e352f7fc25f0ff3ecbea4bfc35b
Nicolas Boutet <amd3002@gmail.com>
2012-11-29 19:53:52 +0100

    game: invalid memset size fix

--- src/game/bg_animation.c
+++ src/game/bg_animation.c
@@ -604,7 +604,8 @@ BG_ParseConditionBits
   convert the string into a single int containing bit flags, stopping at a ',' or end of line
 =================
 */
-void BG_ParseConditionBits(char **text_pp, animStringItem_t *stringTable, int condIndex, int result[2])
+#define RESULT_SIZE 2
+void BG_ParseConditionBits(char **text_pp, animStringItem_t *stringTable, int condIndex, int result[RESULT_SIZE])
 {
 	qboolean           endFlag = qfalse;
 	int                indexFound;
@@ -615,7 +616,7 @@ void BG_ParseConditionBits(char **text_pp, animStringItem_t *stringTable, int co
 
 	//indexBits = 0;
 	currentString[0] = '\0';
-	memset(result, 0, sizeof(result));
+	memset(result, 0, sizeof(result[0]) * RESULT_SIZE);
 	memset(tempBits, 0, sizeof(tempBits));
 
 	while (!endFlag)
