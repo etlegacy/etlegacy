76063548c8776a4c6442cc53066ed972efe1429d
Radegast <jsimek.cz@gmail.com>
2012-12-05 00:09:23 +0000

    generic: fixed compilation with pdcurses console, fixes #140

--- src/sys/con_tty.c
+++ src/sys/con_tty.c
@@ -12,6 +12,15 @@
 #include <fcntl.h>
 #include <sys/time.h>
 
+/* fallbacks for con_curses.c */
+#ifdef FEATURE_CURSES
+#define CON_Init CON_Init_tty
+#define CON_Shutdown CON_Shutdown_tty
+#define CON_Print CON_Print_tty
+#define CON_Input CON_Input_tty
+#define CON_Clear_f Field_Clear(&TTY_con)
+#endif
+
 /*
 =============================================================
 tty console routines
