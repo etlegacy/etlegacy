aaedf1265439582505f15028fe51ca92951ec7e3
Radegast <jsimek.cz@gmail.com>
2013-03-28 20:40:15 +0100

    client: added 'clean' command which removes files matching a given pattern from fs_homepath, refs #53

--- src/client/client.h
+++ src/client/client.h
@@ -470,6 +470,8 @@ int CL_ServerStatus(char *serverAddress, char *serverStatusString, int maxLen);
 void CL_AddToLimboChat(const char *str);
 qboolean CL_GetLimboString(int index, char *buf);
 
+void CL_CleanHomepath_f(void);
+
 // cl_avi
 
 qboolean CL_OpenAVIForWriting(const char *filename);
--- src/qcommon/qcommon.h
+++ src/qcommon/qcommon.h
@@ -630,6 +630,7 @@ void FS_FreeFileList(char **list);
 qboolean FS_FileExists(const char *file);
 
 qboolean FS_CreatePath(char *OSPath);
+void FS_Remove(const char *osPath);
 
 char *FS_BuildOSPath(const char *base, const char *game, const char *qpath);
 
@@ -1254,5 +1255,3 @@ extern huffman_t clientHuffTables;
 #define CL_DECODE_START     4
 
 void Com_GetHunkInfo(int *hunkused, int *hunkexpected);
-
-#endif // _QCOMMON_H_
