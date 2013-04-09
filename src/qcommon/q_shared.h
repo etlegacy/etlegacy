f03406bfb4b33cc67c93ea8ce846fddfb1125f2d
Radegast <jsimek.cz@gmail.com>
2013-04-09 16:42:00 +0200

    client: further compilation fix on Windows, refs #172

--- src/qcommon/q_shared.h
+++ src/qcommon/q_shared.h
@@ -148,10 +148,10 @@ typedef int intptr_t;
 #ifdef _MSC_VER
 #include <io.h>
 
-typedef __int64 int64_t;
-typedef __int32 int32_t;
-typedef __int16 int16_t;
-typedef __int8 int8_t;
+typedef signed __int64 int64_t;
+typedef signed __int32 int32_t;
+typedef signed __int16 int16_t;
+typedef signed __int8 int8_t;
 typedef unsigned __int64 uint64_t;
 typedef unsigned __int32 uint32_t;
 typedef unsigned __int16 uint16_t;
