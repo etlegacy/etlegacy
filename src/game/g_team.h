822d7cc1057fd36e81311a1ce0d505867009ad96
IR4T4 <gutzu@gmx.ch>
2013-10-06 06:38:26 +0200

    game: fixed missing break in PM_CheckProne, clean up & switches

--- src/game/g_team.h
+++ src/game/g_team.h
@@ -72,32 +72,10 @@
 #define EXPLOSIVE_TOUCHABLE         2
 #define EXPLOSIVE_USESHADER         4
 #define EXPLOSIVE_LOWGRAV           8
-#define EXPLOSIVE_NO_AAS_BLOCKING   16
+//#define EXPLOSIVE_NO_AAS_BLOCKING   16 // obsolete (might be used in older maps)
 #define EXPLOSIVE_TANK              32
 
 #define CTF_TARGET_PROTECT_RADIUS           400 // the radius around an object being defended where a target will be worth extra frags
-
-/*#define CTF_CAPTURE_BONUS		5		// what you get for capture
-#define CTF_TEAM_BONUS			0		// what your team gets for capture
-#define CTF_RECOVERY_BONUS		1		// what you get for recovery
-#define CTF_FLAG_BONUS			0		// what you get for picking up enemy flag
-#define CTF_FRAG_CARRIER_BONUS	2		// what you get for fragging enemy flag carrier
-#define CTF_FLAG_RETURN_TIME	40000	// seconds until auto return
-
-#define CTF_CARRIER_DANGER_PROTECT_BONUS	2	// bonus for fraggin someone who has recently hurt your flag carrier
-#define CTF_CARRIER_PROTECT_BONUS			1	// bonus for fraggin someone while either you or your target are near your flag carrier
-#define CTF_FLAG_DEFENSE_BONUS				1	// bonus for fraggin someone while either you or your target are near your flag
-#define CTF_RETURN_FLAG_ASSIST_BONUS		1	// awarded for returning a flag that causes a capture to happen almost immediately
-#define CTF_FRAG_CARRIER_ASSIST_BONUS		2	// award for fragging a flag carrier if a capture happens almost immediately
-
-#define CTF_ATTACKER_PROTECT_RADIUS			400	// the radius around an object being defended where an attacker will get extra frags when making kills
-
-#define CTF_CARRIER_DANGER_PROTECT_TIMEOUT	8
-#define CTF_FRAG_CARRIER_ASSIST_TIMEOUT		10000
-#define CTF_RETURN_FLAG_ASSIST_TIMEOUT		10000
-
-#define CTF_GRAPPLE_SPEED					750 // speed of grapple in flight
-#define CTF_GRAPPLE_PULL_SPEED				750	// speed player is pulled at*/
 
 // Prototypes
 
@@ -106,7 +84,6 @@ const char *TeamName(int team);
 const char *OtherTeamName(int team);
 const char *TeamColorString(int team);
 
-void Team_RemoveFlag(int team);
 void Team_DroppedFlagThink(gentity_t *ent);
 void Team_FragBonuses(gentity_t *targ, gentity_t *inflictor, gentity_t *attacker);
 
