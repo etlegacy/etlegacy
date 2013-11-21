bd7576cfac4b82b131c6de8b33641cda4272190b
IR4T4 <gutzu@gmx.ch>
2013-11-21 21:10:29 +0100

    game: workaround for levelTime starts over when timelimit expires on dual objective maps - patch by tjw/etpub

--- src/game/g_local.h
+++ src/game/g_local.h
@@ -910,6 +910,7 @@ typedef struct level_locals_s
 
 	int framenum;
 	int time;                           // in msec
+	int overTime;                       // workaround for dual objective timelimit bug
 	int previousTime;                   // so movers can back up when blocked
 	int frameTime;                      // time the frame started, for antilag stuff
 
