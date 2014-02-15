8be99042e965642603ec03a657543bb0aaf31c19
Remy Marquis <remy.marquis@gmail.com>
2014-02-15 16:40:39 +0100

    client: fixed console color codes input, refs #412, refs #502

--- src/client/cl_console.c
+++ src/client/cl_console.c
@@ -462,11 +462,8 @@ void CL_ConsolePrint(char *txt)
 
 	if (!con.initialized)
 	{
-		con.color[0]             =
-		    con.color[1]         =
-		        con.color[2]     =
-		            con.color[3] = 1.0f;
-		con.linewidth            = -1;
+		con.color[0]  = con.color[1] = con.color[2] = con.color[3] = 1.0f;
+		con.linewidth = -1;
 		Con_CheckResize();
 		con.initialized = qtrue;
 	}
@@ -599,7 +596,7 @@ void Con_DrawInput(void)
 	SCR_DrawSmallChar(con.xadjust + 1 * SMALLCHAR_WIDTH, y, ']');
 
 	Field_Draw(&g_consoleField, con.xadjust + 2 * SMALLCHAR_WIDTH, y,
-	           SCREEN_WIDTH - 3 * SMALLCHAR_WIDTH, qtrue, qtrue);
+	           SCREEN_WIDTH - 3 * SMALLCHAR_WIDTH, qtrue, qfalse);
 }
 
 /*
