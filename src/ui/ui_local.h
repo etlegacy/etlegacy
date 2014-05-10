f9d19c5df33af5b77cd0777c8630c6b0663ef46c
IR4T4 <gutzu@gmx.ch>
2014-05-10 14:43:16 +0200

    ui: MAX_MAPS increased

--- src/ui/ui_local.h
+++ src/ui/ui_local.h
@@ -295,7 +295,7 @@ extern void UI_DrawLoadPanel(qboolean ownerdraw, qboolean uihack);
 #define MAX_HEADNAME  32
 #define MAX_TEAMS 64
 
-#define MAX_MAPS 128
+#define MAX_MAPS 256
 #define MAX_SPMAPS 16
 #define PLAYERS_PER_TEAM 5
 #define MAX_PINGREQUESTS        16
