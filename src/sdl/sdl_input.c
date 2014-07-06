bba5e20792de80f7b89d602ab19627117e9a9f66
ZTM <zturtleman@gmail.com>
2014-07-06 12:26:32 +0200

    sdl: Remove unused array joy_pressed

--- src/sdl/sdl_input.c
+++ src/sdl/sdl_input.c
@@ -785,7 +785,6 @@ static void IN_ShutdownJoystick(void)
 
 static void IN_JoyMove(void)
 {
-	qboolean     joy_pressed[ARRAY_LEN(joy_keys)];
 	unsigned int axes  = 0;
 	unsigned int hats  = 0;
 	int          total = 0;
@@ -797,8 +796,6 @@ static void IN_JoyMove(void)
 	}
 
 	SDL_JoystickUpdate();
-
-	memset(joy_pressed, '\0', sizeof(joy_pressed));
 
 	// update the ball state.
 	total = SDL_JoystickNumBalls(stick);
