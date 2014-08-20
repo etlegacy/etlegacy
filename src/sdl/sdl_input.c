b5751b97dfe1ae4d4ccdc14f693668b202af8cf4
IR4T4 <gutzu@gmx.ch>
2014-08-20 19:40:31 +0200

    sdl: SDLToQ3Key mapping optimized. Don't map to keypad control keys when we want numbers refs #558

--- src/sdl/sdl_input.c
+++ src/sdl/sdl_input.c
@@ -279,46 +279,123 @@ static keyNum_t IN_TranslateSDLToQ3Key(SDL_Keysym *keysym, qboolean down)
 	}
 	else
 	{
+/*
+        // FIXME/SDL2 BUG/FEATUTE NUM keyboard state
+        // https://forums.libsdl.org/viewtopic.php?p=43816&sid=b2ff6e037c85e6a80c6c506035668246
+        // currently SDL2 only sets NUM keyboard status when key is pressed and expects NUM disabled at start
+        // see SDL_SendKeyboardKey in SDL_Keyboard.c
+        // notes:
+        // - SDL_SCANCODE_NUMLOCKCLEAR does the job
+        // - clarify APPLE clear key
+        if (SDL_GetModState() & KMOD_NUM)
+        {
+            Com_Printf("NUM LED on\n");
+        }
+        else
+        {
+            Com_Printf("NUM LED off\n");
+        }
+*/
+
 		switch (keysym->sym)
 		{
-		case SDLK_PAGEUP:       key  = K_PGUP;          break;
-		case SDLK_KP_9:          key = K_KP_PGUP;       break;
-		case SDLK_PAGEDOWN:     key  = K_PGDN;          break;
-		case SDLK_KP_3:          key = K_KP_PGDN;       break;
-		case SDLK_KP_7:          key = K_KP_HOME;       break;
-		case SDLK_HOME:         key  = K_HOME;          break;
-		case SDLK_KP_1:          key = K_KP_END;        break;
-		case SDLK_END:          key  = K_END;           break;
-		case SDLK_KP_4:          key = K_KP_LEFTARROW;  break;
-		case SDLK_LEFT:         key  = K_LEFTARROW;     break;
-		case SDLK_KP_6:          key = K_KP_RIGHTARROW; break;
-		case SDLK_RIGHT:        key  = K_RIGHTARROW;    break;
-		case SDLK_KP_2:          key = K_KP_DOWNARROW;  break;
-		case SDLK_DOWN:         key  = K_DOWNARROW;     break;
-		case SDLK_KP_8:          key = K_KP_UPARROW;    break;
-		case SDLK_UP:           key  = K_UPARROW;       break;
-		case SDLK_ESCAPE:       key  = K_ESCAPE;        break;
-		case SDLK_KP_ENTER:     key  = K_KP_ENTER;      break;
-		case SDLK_RETURN:       key  = K_ENTER;         break;
-		case SDLK_TAB:          key  = K_TAB;           break;
-		case SDLK_F1:           key  = K_F1;            break;
-		case SDLK_F2:           key  = K_F2;            break;
-		case SDLK_F3:           key  = K_F3;            break;
-		case SDLK_F4:           key  = K_F4;            break;
-		case SDLK_F5:           key  = K_F5;            break;
-		case SDLK_F6:           key  = K_F6;            break;
-		case SDLK_F7:           key  = K_F7;            break;
-		case SDLK_F8:           key  = K_F8;            break;
-		case SDLK_F9:           key  = K_F9;            break;
-		case SDLK_F10:          key  = K_F10;           break;
-		case SDLK_F11:          key  = K_F11;           break;
-		case SDLK_F12:          key  = K_F12;           break;
-		case SDLK_F13:          key  = K_F13;           break;
-		case SDLK_F14:          key  = K_F14;           break;
-		case SDLK_F15:          key  = K_F15;           break;
 
-		case SDLK_BACKSPACE:    key = K_BACKSPACE;     break;
+		// keypad
+		case SDLK_KP_0:
+			if (!(keysym->mod & KMOD_NUM))
+			{
+				key = K_KP_INS;
+			}
+			break;
+		case SDLK_KP_1:
+			if (!(keysym->mod & KMOD_NUM))
+			{
+				key = K_KP_END;
+			}
+			break;
+		case SDLK_KP_2:
+			if (!(keysym->mod & KMOD_NUM))
+			{
+				key = K_KP_DOWNARROW;
+			}
+			break;
+		case SDLK_KP_3:
+			if (!(keysym->mod & KMOD_NUM))
+			{
+				key = K_KP_PGDN;
+			}
+			break;
+		case SDLK_KP_4:
+			if (!(keysym->mod & KMOD_NUM))
+			{
+				key = K_KP_LEFTARROW;
+			}
+			break;
+		case SDLK_KP_5:
+			if (!(keysym->mod & KMOD_NUM))
+			{
+				key = K_KP_5;
+			}
+			break;
+		case SDLK_KP_6:
+			if (!(keysym->mod & KMOD_NUM))
+			{
+				key = K_KP_RIGHTARROW;
+			}
+			break;
+		case SDLK_KP_7:
+			if (!(keysym->mod & KMOD_NUM))
+			{
+				key = K_KP_HOME;
+			}
+			break;
+		case SDLK_KP_8:
+			if (!(keysym->mod & KMOD_NUM))
+			{
+				key = K_KP_UPARROW;
+			}
+			break;
+		case SDLK_KP_9:
+			if (!(keysym->mod & KMOD_NUM))
+			{
+				key = K_KP_PGUP;
+			}
+			break;
+		case SDLK_KP_ENTER:     key = K_KP_ENTER;      break;
 		case SDLK_KP_PERIOD:    key = K_KP_DEL;        break;
+		case SDLK_KP_MULTIPLY:  key = K_KP_STAR;       break;
+		case SDLK_KP_PLUS:      key = K_KP_PLUS;       break;
+		case SDLK_KP_MINUS:     key = K_KP_MINUS;      break;
+		case SDLK_KP_DIVIDE:    key = K_KP_SLASH;      break;
+
+		case SDLK_PAGEUP:       key = K_PGUP;          break;
+		case SDLK_PAGEDOWN:     key = K_PGDN;          break;
+		case SDLK_HOME:         key = K_HOME;          break;
+		case SDLK_END:          key = K_END;           break;
+		case SDLK_LEFT:         key = K_LEFTARROW;     break;
+		case SDLK_RIGHT:        key = K_RIGHTARROW;    break;
+		case SDLK_DOWN:         key = K_DOWNARROW;     break;
+		case SDLK_UP:           key = K_UPARROW;       break;
+		case SDLK_ESCAPE:       key = K_ESCAPE;        break;
+		case SDLK_RETURN:       key = K_ENTER;         break;
+		case SDLK_TAB:          key = K_TAB;           break;
+		case SDLK_F1:           key = K_F1;            break;
+		case SDLK_F2:           key = K_F2;            break;
+		case SDLK_F3:           key = K_F3;            break;
+		case SDLK_F4:           key = K_F4;            break;
+		case SDLK_F5:           key = K_F5;            break;
+		case SDLK_F6:           key = K_F6;            break;
+		case SDLK_F7:           key = K_F7;            break;
+		case SDLK_F8:           key = K_F8;            break;
+		case SDLK_F9:           key = K_F9;            break;
+		case SDLK_F10:          key = K_F10;           break;
+		case SDLK_F11:          key = K_F11;           break;
+		case SDLK_F12:          key = K_F12;           break;
+		case SDLK_F13:          key = K_F13;           break;
+		case SDLK_F14:          key = K_F14;           break;
+		case SDLK_F15:          key = K_F15;           break;
+		case SDLK_BACKSPACE:    key = K_BACKSPACE;     break;
+
 		case SDLK_DELETE:       key = K_DEL;           break;
 		case SDLK_PAUSE:        key = K_PAUSE;         break; // @todo SDL 2.0 maps PAUSE to PAUSE as well as BREAK (key = K_BREAK;         break;)
 
@@ -339,38 +416,30 @@ static keyNum_t IN_TranslateSDLToQ3Key(SDL_Keysym *keysym, qboolean down)
 #endif
 
 		case SDLK_RGUI:
-		case SDLK_LGUI:        key = K_COMMAND;       break; // @todo SDL 2.0 maps GUI to SUPER as well as COMMAND (key = K_SUPER;         break;)
+		case SDLK_LGUI:         key = K_COMMAND;       break; // FIXME: SDL 2.0 maps GUI to SUPER as well as COMMAND (key = K_SUPER;         break;)
 
 		case SDLK_RALT:
 		case SDLK_LALT:         key = K_ALT;           break;
 
+		case SDLK_INSERT:       key = K_INS;           break;
 
-		case SDLK_KP_5:          key = K_KP_5;          break;
-		case SDLK_INSERT:       key  = K_INS;           break;
-		case SDLK_KP_0:          key = K_KP_INS;        break;
-		case SDLK_KP_MULTIPLY:  key  = K_KP_STAR;       break;
-		case SDLK_KP_PLUS:      key  = K_KP_PLUS;       break;
-		case SDLK_KP_MINUS:     key  = K_KP_MINUS;      break;
-		case SDLK_KP_DIVIDE:    key  = K_KP_SLASH;      break;
-
-		case SDLK_MODE:         key         = K_MODE;          break;
-		case SDLK_APPLICATION:      key     = K_COMPOSE;       break;
-		case SDLK_HELP:         key         = K_HELP;          break;
-		case SDLK_PRINTSCREEN:  key         = K_PRINT;         break;
-		case SDLK_SYSREQ:       key         = K_SYSREQ;        break;
-		case SDLK_MENU:         key         = K_MENU;          break;
-		case SDLK_POWER:        key         = K_POWER;         break;
-		case SDLK_CURRENCYUNIT:         key = K_EURO;          break;
-		case SDLK_UNDO:         key         = K_UNDO;          break;
-		case SDLK_SCROLLLOCK:    key        = K_SCROLLOCK;     break;
-		case SDLK_NUMLOCKCLEAR:      key    = K_KP_NUMLOCK;    break;
-		case SDLK_CAPSLOCK:     key         = K_CAPSLOCK;      break;
+		case SDLK_MODE:         key = K_MODE;          break;
+		case SDLK_APPLICATION:  key = K_COMPOSE;       break;
+		case SDLK_HELP:         key = K_HELP;          break;
+		case SDLK_PRINTSCREEN:  key = K_PRINT;         break;
+		case SDLK_SYSREQ:       key = K_SYSREQ;        break;
+		case SDLK_MENU:         key = K_MENU;          break;
+		case SDLK_POWER:        key = K_POWER;         break;
+		case SDLK_CURRENCYUNIT: key = K_EURO;          break;
+		case SDLK_UNDO:         key = K_UNDO;          break;
+		case SDLK_SCROLLLOCK:   key = K_SCROLLOCK;     break;
+		case SDLK_NUMLOCKCLEAR: key = K_KP_NUMLOCK;    break;
+		case SDLK_CAPSLOCK:     key = K_CAPSLOCK;      break;
 
 		default:
-			/* TODO: SDL 2.0
-			if (keysym->sym >= SDLK_WORLD_0 && keysym->sym <= SDLK_WORLD_95)
-			  key = (keysym->sym - SDLK_WORLD_0) + K_WORLD_0;
-			*/
+			// TODO: SDL 2.0
+			//if (keysym->sym >= SDLK_WORLD_0 && keysym->sym <= SDLK_WORLD_95)
+			//key = (keysym->sym - SDLK_WORLD_0) + K_WORLD_0;
 			break;
 		}
 	}
@@ -395,6 +464,7 @@ static keyNum_t IN_TranslateSDLToQ3Key(SDL_Keysym *keysym, qboolean down)
 	//return (char *)buf;
 	return key;
 }
+
 
 static void IN_GobbleMotionEvents(void)
 {
