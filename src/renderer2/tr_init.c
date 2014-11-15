838b0184ec7786464db49be797facf1d9bd6e678
IR4T4 <gutzu@gmx.ch>
2014-11-16 00:20:16 +0100

    renderer2: sync r_drawSun default value with r1

--- src/renderer2/tr_init.c
+++ src/renderer2/tr_init.c
@@ -1295,7 +1295,7 @@ void R_Register(void)
 	r_ignoreGLErrors = ri.Cvar_Get("r_ignoreGLErrors", "1", CVAR_ARCHIVE);
 	r_fastsky        = ri.Cvar_Get("r_fastsky", "0", CVAR_ARCHIVE);
 
-	r_drawSun       = ri.Cvar_Get("r_drawSun", "0", CVAR_ARCHIVE);
+	r_drawSun       = ri.Cvar_Get("r_drawSun", "1", CVAR_ARCHIVE);
 	r_finish        = ri.Cvar_Get("r_finish", "0", CVAR_CHEAT);
 	r_textureMode   = ri.Cvar_Get("r_textureMode", "GL_LINEAR_MIPMAP_NEAREST", CVAR_ARCHIVE);
 	r_swapInterval  = ri.Cvar_Get("r_swapInterval", "0", CVAR_ARCHIVE);
