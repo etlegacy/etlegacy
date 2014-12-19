0e291388106c8b2fc10e2d7d094f1415a11b2ae6
IR4T4 <gutzu@gmx.ch>
2014-12-19 14:34:28 +0100

    client: Mu-Law decompress lookup table

--- src/client/snd_wavelet.c
+++ src/client/snd_wavelet.c
@@ -132,21 +132,41 @@ byte MuLawEncode(short s)
 	return ~(sign | (exponent << 4) | mantissa);
 }
 
-short MuLawDecode(byte uLaw)
+short mulawToShort[256] =
 {
-	signed long adjusted;
-	byte        exponent, mantissa;
-
-	uLaw     = ~uLaw;
-	exponent = (uLaw >> 4) & 0x7;
-	mantissa = (uLaw & 0xf) + 16;
-	adjusted = (mantissa << (exponent + 3)) - 128 - 4;
-
-	return (uLaw & 0x80) ? adjusted : -adjusted;
-}
-
-short           mulawToShort[256];
-static qboolean madeTable = qfalse;
+	-32124, -31100, -30076, -29052, -28028, -27004, -25980, -24956,
+	-23932, -22908, -21884, -20860, -19836, -18812, -17788, -16764,
+	-15996, -15484, -14972, -14460, -13948, -13436, -12924, -12412,
+	-11900, -11388, -10876, -10364, -9852,  -9340,  -8828,  -8316,
+	-7932,  -7676,  -7420,  -7164,  -6908,  -6652,  -6396,  -6140,
+	-5884,  -5628,  -5372,  -5116,  -4860,  -4604,  -4348,  -4092,
+	-3900,  -3772,  -3644,  -3516,  -3388,  -3260,  -3132,  -3004,
+	-2876,  -2748,  -2620,  -2492,  -2364,  -2236,  -2108,  -1980,
+	-1884,  -1820,  -1756,  -1692,  -1628,  -1564,  -1500,  -1436,
+	-1372,  -1308,  -1244,  -1180,  -1116,  -1052,  -988,   -924,
+	-876,   -844,   -812,   -780,   -748,   -716,   -684,   -652,
+	-620,   -588,   -556,   -524,   -492,   -460,   -428,   -396,
+	-372,   -356,   -340,   -324,   -308,   -292,   -276,   -260,
+	-244,   -228,   -212,   -196,   -180,   -164,   -148,   -132,
+	-120,   -112,   -104,   -96,    -88,    -80,    -72,    -64,
+	-56,    -48,    -40,    -32,    -24,    -16,    -8,     -1,
+	32124,  31100,  30076,  29052,  28028,  27004,  25980,  24956,
+	23932,  22908,  21884,  20860,  19836,  18812,  17788,  16764,
+	15996,  15484,  14972,  14460,  13948,  13436,  12924,  12412,
+	11900,  11388,  10876,  10364,  9852,   9340,   8828,   8316,
+	7932,   7676,   7420,   7164,   6908,   6652,   6396,   6140,
+	5884,   5628,   5372,   5116,   4860,   4604,   4348,   4092,
+	3900,   3772,   3644,   3516,   3388,   3260,   3132,   3004,
+	2876,   2748,   2620,   2492,   2364,   2236,   2108,   1980,
+	1884,   1820,   1756,   1692,   1628,   1564,   1500,   1436,
+	1372,   1308,   1244,   1180,   1116,   1052,   988,    924,
+	876,    844,    812,    780,    748,    716,    684,    652,
+	620,    588,    556,    524,    492,    460,    428,    396,
+	372,    356,    340,    324,    308,    292,    276,    260,
+	244,    228,    212,    196,    180,    164,    148,    132,
+	120,    112,    104,    96,     88,     80,     72,     64,
+	56,     48,     40,     32,     24,     16,     8,      0
+};
 
 void encodeWavelet(sfx_t *sfx, short *packets)
 {
@@ -154,15 +174,6 @@ void encodeWavelet(sfx_t *sfx, short *packets)
 	int       i, samples = sfx->soundLength, size;
 	sndBuffer *newchunk, *chunk = NULL;
 	byte      *out;
-
-	if (!madeTable)
-	{
-		for (i = 0; i < 256; i++)
-		{
-			mulawToShort[i] = (float)MuLawDecode((byte)i);
-		}
-		madeTable = qtrue;
-	}
 
 	while (samples > 0)
 	{
@@ -245,15 +256,6 @@ void encodeMuLaw(sfx_t *sfx, short *packets)
 	sndBuffer *newchunk, *chunk = NULL;
 	byte      *out;
 
-	if (!madeTable)
-	{
-		for (i = 0; i < 256; i++)
-		{
-			mulawToShort[i] = (float)MuLawDecode((byte)i);
-		}
-		madeTable = qtrue;
-	}
-
 	while (samples > 0)
 	{
 		size = samples;
@@ -292,17 +294,3 @@ void encodeMuLaw(sfx_t *sfx, short *packets)
 		samples    -= size;
 	}
 }
-
-/* @brief Unused
-void decodeMuLaw(sndBuffer *chunk, short *to)
-{
-    int  i;
-    int  size = chunk->size;
-    byte *out = (byte *)chunk->sndChunk;
-
-    for (i = 0; i < size; i++)
-    {
-        to[i] = mulawToShort[out[i]];
-    }
-}
-*/
