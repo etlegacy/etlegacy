4d1a0abb11c589acf1dd1ae77fe9194a20c35aa7
jackeri <shogaiam@gmail.com>
2014-12-22 12:20:47 +0200

    fixed the git version issue with the windows easybuild and also changed the build to msbuild to enable the XP support toolchain

--- easybuild.bat
+++ easybuild.bat
@@ -12,50 +12,50 @@ ECHO ETLegacy easybuild for Windows
 :: The default VS version
 set vsversion=12
 set vsvarsbat=!VS%vsversion%0COMNTOOLS!\vsvars32.bat
-:: Setup the NMake env or find the correct .bat
+:: Setup the NMake env or find the correct .bat this also finds msbuild
 CALL:SETUPNMAKE
-:: CALL "C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\Tools\VsDevCmd.bat" x86
 
 :: variables
-SET homepath=%USERPROFILE%\Documents\ETLegacy
-SET basepath=%USERPROFILE%\Documents\ETLegacy-Build
-SET build=Debug
+SET game_homepath=%USERPROFILE%\Documents\ETLegacy
+SET game_basepath=%USERPROFILE%\Documents\ETLegacy-Build
+SET build=Release
 
 PAUSE
 
 :: clean
 ECHO Cleaning...
-IF EXIST %basepath% RMDIR /s /q %basepath%
+IF EXIST %game_basepath% RMDIR /s /q %game_basepath%
 IF EXIST build RMDIR /s /q build
-DEL /q %homepath%\legacy\*.pk3
-DEL /q %homepath%\legacy\*.dll
-DEL /q %homepath%\legacy\*.dat
+DEL /q %game_homepath%\legacy\*.pk3
+DEL /q %game_homepath%\legacy\*.dll
+DEL /q %game_homepath%\legacy\*.dat
 
 :: build
 ECHO Building...
 MKDIR build
 CD build
 
-cmake -G "NMake Makefiles" -DBUNDLED_LIBS=YES ^
+cmake -G "Visual Studio %vsversion%" -T v%vsversion%0_xp -DBUNDLED_LIBS=YES ^
 -DCMAKE_BUILD_TYPE=%build% ^
--DCMAKE_INSTALL_PREFIX=%basepath% ^
+-DCMAKE_INSTALL_PREFIX=%game_basepath% ^
 -DINSTALL_DEFAULT_BASEDIR=./ ^
 -DINSTALL_DEFAULT_BINDIR=./ ^
 -DINSTALL_DEFAULT_MODDIR=./ ^
 -DINSTALL_OMNIBOT=YES ^
+-DCROSS_COMPILE32=YES ^
 ..
 
-nmake
+msbuild ETLEGACY.sln /target:ALL_BUILD /p:Configuration=Release
 
 :: install
 ECHO Installing...
-nmake install
+msbuild ETLEGACY.sln /target:INSTALL /p:Configuration=Release
 
 :: FIXME: SDL2.dll isn't copied by CMake (why?)
-COPY SDL2.dll %basepath%\SDL2.dll
+COPY SDL2.dll %game_basepath%\SDL2.dll
 
 :: done
-ECHO The %build% build has been installed in %basepath%.
+ECHO The %build% build has been installed in %game_basepath%.
 PAUSE
 GOTO:EOF
 
