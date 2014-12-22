26b43b428782fb299135fc67258e453e593fd84e
IR4T4 <gutzu@gmx.ch>
2014-12-22 18:09:39 +0100

    game: fix out of bounds

--- src/game/bg_stats.c
+++ src/game/bg_stats.c
@@ -107,7 +107,7 @@ static const weap_ws_convert_t aWeapID[WP_NUM_WEAPONS] =
 // Get right stats index based on weapon id
 extWeaponStats_t BG_WeapStatForWeapon(weapon_t iWeaponID)
 {
-	if (iWeaponID <= WP_NUM_WEAPONS)
+	if (iWeaponID < WP_NUM_WEAPONS)
 	{
 		return aWeapID[iWeaponID].iWS;
 	}
