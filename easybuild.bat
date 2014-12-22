76be4b955aa981fce4eb1bdead8520df6d5878ac
jackeri <shogaiam@gmail.com>
2014-12-22 14:34:10 +0200

    pimped the easybuild a bit further, its now possible to run just parts of the script

--- easybuild.bat
+++ easybuild.bat
@@ -7,10 +7,6 @@
 @echo off
 @setLocal EnableDelayedExpansion
 
-TITLE Building ET:Legacy
-
-ECHO ETLegacy easybuild for Windows
-
 :: The default VS version
 set vsversion=12
 set vsvarsbat=!VS%vsversion%0COMNTOOLS!\vsvars32.bat
@@ -20,47 +16,37 @@ CALL:SETUPNMAKE
 :: variables
 SET game_homepath=%USERPROFILE%\Documents\ETLegacy
 SET game_basepath=%USERPROFILE%\Documents\ETLegacy-Build
-SET build=Release
+SET build_type=Release
 SET batloc=%~dp0
 SET build_dir=%batloc%build
 
-:: clean
-ECHO Cleaning...
-IF EXIST %game_basepath% RMDIR /s /q %game_basepath%
-IF EXIST %build_dir% RMDIR /s /q %build_dir%
-
-:: build
-ECHO Building...
-MKDIR %build_dir%
-CD %build_dir%
-
-cmake -G "Visual Studio %vsversion%" -T v%vsversion%0_xp -DBUNDLED_LIBS=YES ^
--DCMAKE_BUILD_TYPE=%build% ^
--DCMAKE_INSTALL_PREFIX=%game_basepath% ^
--DINSTALL_DEFAULT_BASEDIR=./ ^
--DINSTALL_DEFAULT_BINDIR=./ ^
--DINSTALL_DEFAULT_MODDIR=./ ^
--DINSTALL_OMNIBOT=YES ^
--DCROSS_COMPILE32=YES ^
-..
-
-msbuild ETLEGACY.sln /target:ALL_BUILD /p:Configuration=Release
-
-if errorlevel 1 (
-	ECHO There was an issue with the build so no files are copied
-	PAUSE
-	GOTO:EOF
+IF "%~1"=="" (
+	CALL:DEFAULTPROCESS
+) ELSE (
+	GOTO:PROCESSARGS
 )
+GOTO:EOF
 
-:: install
-ECHO Setting up the game
-CALL:SETUPFOLDERS
+:: process command line arguments if any
+:PROCESSARGS
+	FOR %%A IN (%*) DO (
+		IF errorlevel 1 (
+			echo Failure errorlevel: %errorlevel%
+			GOTO:EOF
+		) ELSE (
+			CALL:FUNCTIONS %%A
+		)
+	)
+GOTO:EOF
 
-:: done
-CALL:CRETELINK "ETLegacy" "%game_basepath%\etl.exe" "%game_basepath%"
-ECHO The %build% build has been installed in %game_basepath%, and shortcut has been added to your desktop
-SLEEP 5
-::explorer %game_basepath%
+:FUNCTIONS
+	set curvar=%~1
+	IF /I "%curvar%"=="clean" CALL:DOCLEAN
+	IF /I "%curvar%"=="build" CALL:DOBUILD
+	IF /I "%curvar%"=="install" CALL:DOINSTALL
+	:: download pak0 - 2 to the homepath if they do not exist
+	IF /I "%curvar%"=="download" CALL:DOWNLOADPAKS "http://mirror.etlegacy.com/etmain/"
+	IF /I "%curvar%"=="open" explorer %game_basepath%
 GOTO:EOF
 
 :SETUPNMAKE
@@ -121,6 +107,58 @@ GOTO:EOF
 	IF EXIST %~1 XCOPY %~1 %~2 /Y >nul
 GOTO:EOF
 
+:DEFAULTPROCESS
+	TITLE Building ET:Legacy
+	ECHO ETLegacy easybuild for Windows
+
+	CALL:DOCLEAN
+	CALL:DOBUILD
+	if errorlevel 1 (
+		ECHO There was an issue with the build so no files are copied
+		PAUSE
+		GOTO:EOF
+	)
+	CALL:DOINSTALL
+	SLEEP 5
+GOTO:EOF
+
+:DOCLEAN
+	:: clean
+	ECHO Cleaning...
+	IF EXIST %game_basepath% RMDIR /s /q %game_basepath%
+	IF EXIST %build_dir% RMDIR /s /q %build_dir%
+GOTO:EOF
+
+:DOBUILD
+	:: build
+	ECHO Building...
+	IF NOT EXIST %build_dir% MKDIR %build_dir%
+	CD %build_dir%
+
+	cmake -G "Visual Studio %vsversion%" -T v%vsversion%0_xp ^
+	-DBUNDLED_LIBS=YES ^
+	-DCMAKE_BUILD_TYPE=%build_type% ^
+	-DCMAKE_INSTALL_PREFIX=%game_basepath% ^
+	-DINSTALL_DEFAULT_BASEDIR=./ ^
+	-DINSTALL_DEFAULT_BINDIR=./ ^
+	-DINSTALL_DEFAULT_MODDIR=./ ^
+	-DINSTALL_OMNIBOT=YES ^
+	-DCROSS_COMPILE32=YES ^
+	..
+
+	msbuild ETLEGACY.sln /target:ALL_BUILD /p:Configuration=%build_type%
+GOTO:EOF
+
+:DOINSTALL
+	:: install
+	ECHO Setting up the game
+	CALL:SETUPFOLDERS
+
+	:: done
+	CALL:CRETELINK "ETLegacy" "%game_basepath%\etl.exe" "%game_basepath%"
+	ECHO The %build_type% build has been installed in %game_basepath%, and shortcut has been added to your desktop
+GOTO:EOF
+
 :CRETELINK
 	set SCRIPT="%TEMP%\%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%.vbs"
 
@@ -133,6 +171,22 @@ GOTO:EOF
 
 	cscript /nologo %SCRIPT%
 	del %SCRIPT%
+GOTO:EOF
+
+:DOWNLOADPAKS
+	IF NOT EXIST "%game_homepath%\etmain" (
+		md "%game_homepath%\etmain"
+	)
+
+	IF NOT EXIST "%game_homepath%\etmain\pak0.pk3" (
+		bitsadmin /transfer "pak0" %~1pak0.pk3 "%game_homepath%\etmain\pak0.pk3"
+	)
+	IF NOT EXIST "%game_homepath%\etmain\pak1.pk3" (
+		bitsadmin /transfer "pak1" %~1pak1.pk3 "%game_homepath%\etmain\pak1.pk3"
+	)
+	IF NOT EXIST "%game_homepath%\etmain\pak2.pk3" (
+		bitsadmin /transfer "pak2" %~1pak2.pk3 "%game_homepath%\etmain\pak2.pk3"
+	)
 GOTO:EOF
 
 :Substring
