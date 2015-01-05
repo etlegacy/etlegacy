90191082b8defd0ff7d1a48a358263aa43ad3030
Radegast <jsimek.cz@gmail.com>
2015-01-05 07:15:59 +0100

    misc: updated POT file generator script

--- misc/pot_generator.sh
+++ misc/pot_generator.sh
@@ -8,11 +8,11 @@
 # CLIENT
 touch etlegacy_client.pot
 find src/server/ src/client/ src/qcommon/ -name '*.c' -o -name '*.cpp' | sort | xgettext \
-\--from-code=UTF-8 --package-name="ET: Legacy" --copyright-holder="Jan Simek" --package-version="2.70rc2" --msgid-bugs-address="bugs@etlegacy.com" -o etlegacy_client.pot -k_ -k__ -kCL_TranslateString -f -
+\--from-code=UTF-8 --package-name="ET: Legacy" --copyright-holder="Jan Simek" --package-version="$(git describe --abbrev=0 --tags)" --msgid-bugs-address="mail@etlegacy.com" -o etmain/locale/etlegacy_client.pot -k_ -k__ -kCL_TranslateString -f -
 
 # MOD
 touch etlegacy_mod.pot
 find src/game/ src/ui/ src/cgame/ -name '*.c' -o -name '*.cpp' -o -name 'missing_translations_*.txt' | sort | xgettext \
-\--from-code=UTF-8 --package-name="ET: Legacy" --copyright-holder="Jan Simek" --package-version="2.70rc2" --msgid-bugs-address="bugs@etlegacy.com" -o etlegacy_mod.pot -k_ -k__ -kCG_TranslateString -ktrap_TranslateString -kTRANSLATE -f -
+\--from-code=UTF-8 --package-name="ET: Legacy" --copyright-holder="Jan Simek" --package-version="$(git describe --abbrev=0 --tags)" --msgid-bugs-address="mail@etlegacy.com" -o etmain/locale/etlegacy_mod.pot -k_ -k__ -kCG_TranslateString -ktrap_TranslateString -kTRANSLATE -f -
 
 # TODO: Menu files
