a5b6b7dd6614ad31e6cee1138870d0ad45c186e0
Radegast <jsimek.cz@gmail.com>
2015-01-05 09:42:17 +0100

    misc: do not replace existing translation template (has to be done manually), refs #244

--- misc/pot_generator.sh
+++ misc/pot_generator.sh
@@ -5,14 +5,18 @@
 # the source code of ET: Legacy
 #
 
+CLIENT_POT=etmain/locale/etlegacy_client.pot
+MOD_POT=etmain/locale/etlegacy_mod.pot 
+VERSION=$(git describe --abbrev=0 --tags)
+
 # CLIENT
-touch etlegacy_client.pot
+touch ${CLIENT_POT}
 find src/server/ src/client/ src/qcommon/ -name '*.c' -o -name '*.cpp' | sort | xgettext \
-\--from-code=UTF-8 --package-name="ET: Legacy" --copyright-holder="Jan Simek" --package-version="$(git describe --abbrev=0 --tags)" --msgid-bugs-address="mail@etlegacy.com" -o etmain/locale/etlegacy_client.pot -k_ -k__ -kCL_TranslateString -f -
+\--join-existing --from-code=UTF-8 --package-name="ET: Legacy" --copyright-holder="Jan Simek" --package-version="${VERSION}" --msgid-bugs-address="mail@etlegacy.com" -o ${CLIENT_POT} -k_ -k__ -kCL_TranslateString -f -
 
 # MOD
-touch etlegacy_mod.pot
+touch ${MOD_POT}
 find src/game/ src/ui/ src/cgame/ -name '*.c' -o -name '*.cpp' -o -name 'missing_translations_*.txt' | sort | xgettext \
-\--from-code=UTF-8 --package-name="ET: Legacy" --copyright-holder="Jan Simek" --package-version="$(git describe --abbrev=0 --tags)" --msgid-bugs-address="mail@etlegacy.com" -o etmain/locale/etlegacy_mod.pot -k_ -k__ -kCG_TranslateString -ktrap_TranslateString -kTRANSLATE -f -
+\--join-existing --from-code=UTF-8 --package-name="ET: Legacy" --copyright-holder="Jan Simek" --package-version="${VERSION}" --msgid-bugs-address="mail@etlegacy.com" -o ${MOD_POT} -k_ -k__ -kCG_TranslateString -ktrap_TranslateString -kTRANSLATE -f -
 
 # TODO: Menu files
