41c5b28a42bd8e8de9949758c4548d3a31856b1e
Radegast <jsimek.cz@gmail.com>
2015-01-10 20:26:33 +0100

    misc: don't invoke gettext with join-existing option

--- misc/pot_generator.sh
+++ misc/pot_generator.sh
@@ -12,12 +12,13 @@ VERSION=$(git describe --abbrev=0 --tags)
 # CLIENT
 touch ${CLIENT_POT}
 find src/server/ src/client/ src/qcommon/ -name '*.c' -o -name '*.cpp' | sort | xgettext \
-\--join-existing --from-code=UTF-8 --package-name="ET: Legacy" --copyright-holder="Jan Simek" --package-version="${VERSION}" --msgid-bugs-address="mail@etlegacy.com" -o ${CLIENT_POT} -k_ -k__ -kCL_TranslateString -f -
+\--from-code=UTF-8 --package-name="ET: Legacy" --copyright-holder="Jan Simek" --package-version="${VERSION}" --msgid-bugs-address="mail@etlegacy.com" -o ${CLIENT_POT} -k_ -k__ -kCL_TranslateString -f -
 
 # MOD
 touch ${MOD_POT}
-find src/game/ src/ui/ src/cgame/ -name '*.c' -o -name '*.cpp' -o -name 'missing_translations_*.txt' | sort | xgettext \
-\--join-existing --from-code=UTF-8 --package-name="ET: Legacy" --copyright-holder="Jan Simek" --package-version="${VERSION}" --msgid-bugs-address="mail@etlegacy.com" -o ${MOD_POT} -k_ -k__ -kCG_TranslateString -ktrap_TranslateString -kTRANSLATE -f -
+find src/game/ src/ui/ src/cgame/ etmain/ui -name '*.c' -o -name '*.cpp' -o -name '*.menu' | sort | xgettext \
+\--language=C --from-code=UTF-8 --package-name="ET: Legacy" --copyright-holder="Jan Simek" --package-version="${VERSION}" --msgid-bugs-address="mail@etlegacy.com" -o ${MOD_POT} -k_ -k__ -kCG_TranslateString -ktrap_TranslateString -kTRANSLATE -f -
+
 
 # TODO: Menu files
 
