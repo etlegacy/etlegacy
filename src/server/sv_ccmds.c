f56e13bd0a4dd6a0c292085ff0d503ce9f4ae86d
Zecklar <zecklar@gmail.com>
2015-03-18 16:11:27 +0200

    general: Block client from entering the game if download in progress, refs #823

--- src/server/sv_ccmds.c
+++ src/server/sv_ccmds.c
@@ -369,16 +369,20 @@ static void SV_MapRestart_f(void)
 			continue;
 		}
 
-		if (client->state == CS_ACTIVE)
+		// Player won't enter the world until the download is done
+		if (client->download == 0)
 		{
-			SV_ClientEnterWorld(client, &client->lastUsercmd);
-		}
-		else
-		{
-			// If we don't reset client->lastUsercmd and are restarting during map load,
-			// the client will hang because we'll use the last Usercmd from the previous map,
-			// which is wrong obviously.
-			SV_ClientEnterWorld(client, NULL);
+			if (client->state == CS_ACTIVE)
+			{
+				SV_ClientEnterWorld(client, &client->lastUsercmd);
+			}
+			else
+			{
+				// If we don't reset client->lastUsercmd and are restarting during map load,
+				// the client will hang because we'll use the last Usercmd from the previous map,
+				// which is wrong obviously.
+				SV_ClientEnterWorld(client, NULL);
+			}
 		}
 	}
 
