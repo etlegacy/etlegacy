248e91ef74675b579656442d5df3fa612e126bda
Remy Marquis <remy.marquis@gmail.com>
2015-04-19 00:17:24 +0200

    client: increased default console width (r_mode 4)

--- src/client/cl_console.c
+++ src/client/cl_console.c
@@ -38,7 +38,7 @@
 #include "../qcommon/q_unicode.h"
 
 #define CONSOLE_COLOR  COLOR_WHITE
-#define DEFAULT_CONSOLE_WIDTH   78
+#define DEFAULT_CONSOLE_WIDTH   99
 
 int g_console_field_width = DEFAULT_CONSOLE_WIDTH;
 
