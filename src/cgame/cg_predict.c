3cbf88337a81e7bded73fae0435f4d6f98cccd6c
IR4T4 <gutzu@gmx.ch>
2015-08-09 13:29:37 +0200

    cgame: don't update the view angle when pmove_fixed is active

--- src/cgame/cg_predict.c
+++ src/cgame/cg_predict.c
@@ -1030,13 +1030,14 @@ void CG_PredictPlayerState(void)
 		// get the previous command
 		trap_GetUserCmd(cmdNum - 1, &cg_pmove.oldcmd);
 
-		if (cg_pmove.pmove_fixed
-				&& !BG_PlayerMounted(cg.snap->ps.eFlags) // don't update view angles - causes issues in 1st person view with weapons using special view
-			    && cg.predictedPlayerState.weapon != WP_MOBILE_MG42_SET && cg.predictedPlayerState.weapon != WP_MOBILE_BROWNING_SET) // see cg_view.c fov_x = 55;
-		{
-			// added tracemask
-			PM_UpdateViewAngles(cg_pmove.ps, cg_pmove.pmext, &cg_pmove.cmd, CG_Trace, cg_pmove.tracemask);
-		}
+
+		//if (cg_pmove.pmove_fixed
+		//  && !BG_PlayerMounted(cg.snap->ps.eFlags) // don't update view angles - causes issues in 1st person view with weapons using special view
+		//  && cg.predictedPlayerState.weapon != WP_MOBILE_MG42_SET && cg.predictedPlayerState.weapon != WP_MOBILE_BROWNING_SET) // see cg_view.c fov_x = 55;
+		//{
+		// added tracemask
+		//  PM_UpdateViewAngles(cg_pmove.ps, cg_pmove.pmext, &cg_pmove.cmd, CG_Trace, cg_pmove.tracemask);
+		//}
 
 		// don't do anything if the time is before the snapshot player time
 		if (cg_pmove.cmd.serverTime <= cg.predictedPlayerState.commandTime)
