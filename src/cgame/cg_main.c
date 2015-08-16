62959ab15fb9f232b7d961e08ca746ed6966a042
IR4T4 <gutzu@gmx.ch>
2015-08-16 08:47:59 +0200

    cgame: unused var int_timescale removed

--- src/cgame/cg_main.c
+++ src/cgame/cg_main.c
@@ -254,7 +254,6 @@ vmCvar_t int_cl_maxpackets;
 vmCvar_t int_cl_timenudge;
 vmCvar_t int_m_pitch;
 vmCvar_t int_sensitivity;
-vmCvar_t int_timescale;
 vmCvar_t int_ui_blackout;
 
 vmCvar_t cg_rconPassword;
@@ -1814,7 +1813,9 @@ static void CG_RegisterGraphics(void)
 	cgs.media.hWeaponEchoSnd_2 = trap_S_RegisterSound("sound/weapons/browning/browning_far.wav", qfalse);
 	cgs.media.hWeaponHeatSnd_2 = trap_S_RegisterSound("sound/weapons/browning/browning_heat.wav", qfalse);
 
-	//cgs.media.hflakWeaponSnd =      trap_S_RegisterSound( "sound/vehicles/misc/20mm_fire.wav", qfalse );
+	// FIXME - find a better sound
+	//cgs.media.hflakWeaponSnd = trap_S_RegisterSound("sound/vehicles/misc/20mm_fire.wav", qfalse);
+	cgs.media.hflakWeaponSnd = trap_S_RegisterSound("sound/vehicles/tank/tank_fire.wav", qfalse);
 
 	cgs.media.minePrimedSound = trap_S_RegisterSound("sound/weapons/landmine/mine_on.wav", qfalse);
 
