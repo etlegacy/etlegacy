29a315a8b5d1ef1248474e43c3fff7a43ae1834d
IR4T4 <gutzu@gmx.ch>
2015-08-30 11:57:32 +0200

    cgame: don't cast same data type

--- src/cgame/cg_effects.c
+++ src/cgame/cg_effects.c
@@ -536,8 +536,7 @@ int CG_GetOriginForTag(centity_t *cent, refEntity_t *parent, char *tagName, int 
 
 	if (axis)
 	{
-		// had to cast away the const to avoid compiler problems...
-		MatrixMultiply(lerped.axis, ((refEntity_t *)parent)->axis, axis);
+		MatrixMultiply(lerped.axis, parent->axis, axis);
 	}
 
 	return retval;
