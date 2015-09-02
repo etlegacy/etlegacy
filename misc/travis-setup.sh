affce108ef3f7f8dcc4f8fa23fd761c0febbca43
Jacker <shogaiam@gmail.com>
2015-09-02 22:39:26 +0300

    restructured the easybuild a bit

--- misc/travis-setup.sh
+++ misc/travis-setup.sh
@@ -13,14 +13,15 @@ elif [[ "$OSTYPE" == "darwin"* ]]; then
 	# we need the gnu-sed from brew for r2
 	echo "Travis running apple build"
 	export BUNDLED_CURL=0
-	export CROSS_COMPILE32=1
-	#export BUNDLED_JPEG=0
+	export BUNDLED_SDL=0
+	export BUNDLED_JPEG=0
 	brew update
 	brew install gnu-sed
-	brew install nasm
-	brew outdated automake || brew upgrade automake
-	brew outdated autoconf || brew upgrade autoconf
-	#brew install jpeg-turbo
+	#brew install nasm
+	#brew outdated automake || brew upgrade automake
+	#brew outdated autoconf || brew upgrade autoconf
+	brew install sdl2
+	brew install jpeg-turbo
 else
 	# unknown system :/
 	echo "Build might fail now"
