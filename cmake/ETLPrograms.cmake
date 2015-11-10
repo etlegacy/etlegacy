3ba998b434e7e3f6373073388daf4291379a48ef
IR4T4 <gutzu@gmx.ch>
2015-11-10 22:18:29 +0100

    misc: sed is only required for r2

--- cmake/ETLPrograms.cmake
+++ cmake/ETLPrograms.cmake
@@ -1,9 +1,11 @@
-# Find GNU sed executable, BSD variant does not work with our script
-if(UNIX AND NOT CMAKE_SYSTEM_NAME MATCHES "Linux")
-	find_program(SED_EXECUTABLE gsed)
-else()
-	find_program(SED_EXECUTABLE sed)
-endif()
+if(FEATURE_RENDERER2)
+	# Find GNU sed executable, BSD variant does not work with our script
+	if(UNIX AND NOT CMAKE_SYSTEM_NAME MATCHES "Linux")
+		find_program(SED_EXECUTABLE gsed)
+	else()
+		find_program(SED_EXECUTABLE sed)
+	endif()
+endif(FEATURE_RENDERER2)
 
 # Enable pk3 creation if zip executable is found. Download it on Windows
 find_program(ZIP_EXECUTABLE zip PATHS ${CMAKE_BINARY_DIR})
