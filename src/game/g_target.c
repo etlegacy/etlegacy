322a8f69d9578d287cde1713afd9bfc3a047d22c
IR4T4 <gutzu@gmx.ch>
2015-12-20 20:02:39 +0100

    game: ensure movers are killed (reverted to old value)

--- src/game/g_target.c
+++ src/game/g_target.c
@@ -755,7 +755,8 @@ If targets, they will be killed when this is fired
 "kill_user_too" will still kill the activator when this ent has targets (default is only kill targets, not activator)
 */
 
-#define TELEFRAG_DAMAGE 500 // FIXME: get exact value for limbo death (kill & send to limbo)
+#define TELEFRAG_DAMAGE_CLIENT 500 // FIXME: get exact value for limbo death (kill & send to limbo)
+#define TELEFRAG_DAMAGE_MOVER 100000
 
 void G_KillEnts(const char *target, gentity_t *ignore, gentity_t *killer, meansOfDeath_t mod)
 {
@@ -774,7 +775,7 @@ void G_KillEnts(const char *target, gentity_t *ignore, gentity_t *killer, meansO
 		// script_movers should die!
 		if (targ->s.eType == ET_MOVER && !Q_stricmp(targ->classname, "script_mover") && targ->die)
 		{
-			G_Damage(targ, killer, killer, NULL, NULL, TELEFRAG_DAMAGE, DAMAGE_NO_PROTECTION, MOD_TELEFRAG);
+			G_Damage(targ, killer, killer, NULL, NULL, TELEFRAG_DAMAGE_MOVER, DAMAGE_NO_PROTECTION, MOD_TELEFRAG);
 			continue;
 		}
 
@@ -801,7 +802,7 @@ void target_kill_use(gentity_t *self, gentity_t *other, gentity_t *activator)
 {
 	if (self->spawnflags & 1)      // kill usertoo
 	{
-		G_Damage(activator, NULL, NULL, NULL, NULL, TELEFRAG_DAMAGE, DAMAGE_NO_PROTECTION, MOD_TELEFRAG);
+		G_Damage(activator, NULL, NULL, NULL, NULL, TELEFRAG_DAMAGE_CLIENT, DAMAGE_NO_PROTECTION, MOD_TELEFRAG);
 	}
 
 	G_KillEnts(self->target, activator, self, MOD_UNKNOWN);
