97bd660db1e94410cae692eb561379884604f0b2
IR4T4 <gutzu@gmx.ch>
2016-08-17 22:34:53 +0200

    misc: let ETL use free fonts

--- src/ui/ui_loadpanel.c
+++ src/ui/ui_loadpanel.c
@@ -166,8 +166,8 @@ void UI_DrawLoadPanel(qboolean ownerdraw, qboolean uihack)
 
 	if (!bg_loadscreeninited)
 	{
-		RegisterFont("ariblk", 27, &uiInfo.uiDC.Assets.bg_loadscreenfont1);
-		RegisterFont("courbd", 30, &uiInfo.uiDC.Assets.bg_loadscreenfont2);
+		RegisterFont("LiberationSans-Bold", 27, &uiInfo.uiDC.Assets.bg_loadscreenfont1);
+		RegisterFont("cour10p_b", 30, &uiInfo.uiDC.Assets.bg_loadscreenfont2);
 
 		BG_PanelButtonsSetup(loadpanelButtons);
 		C_PanelButtonsSetup(loadpanelButtons, Cui_WideXoffset());   // convert to possible widescreen coordinates..
