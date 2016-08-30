668698f88632ea7838db13149ad83fda64dba266
IR4T4 <gutzu@gmx.ch>
2016-08-30 15:46:56 +0200

    qcommon: remove dead variable - patch by UniQP

--- src/qcommon/cm_patch.c
+++ src/qcommon/cm_patch.c
@@ -957,7 +957,7 @@ CM_AddFacetBevels
 void CM_AddFacetBevels(facet_t *facet)
 {
 	int       i, j, k, l;
-	int       axis, dir, order, flipped;
+	int       axis, dir, flipped;
 	float     plane[4], d, minBack, newplane[4];
 	winding_t *w, *w2;
 	vec3_t    mins, maxs, vec, vec2;
@@ -993,10 +993,9 @@ void CM_AddFacetBevels(facet_t *facet)
 	WindingBounds(w, mins, maxs);
 
 	// add the axial planes
-	order = 0;
 	for (axis = 0; axis < 3; axis++)
 	{
-		for (dir = -1; dir <= 1; dir += 2, order++)
+		for (dir = -1; dir <= 1; dir += 2)
 		{
 			VectorClear(plane);
 			plane[axis] = dir;
@@ -1013,7 +1012,7 @@ void CM_AddFacetBevels(facet_t *facet)
 			{
 				continue;
 			}
-			// see if the plane is allready present
+			// see if the plane is already present
 			for (i = 0; i < facet->numBorders; i++)
 			{
 				if (CM_PlaneEqual(&planes[facet->borderPlanes[i]], plane, &flipped))
@@ -1108,7 +1107,7 @@ void CM_AddFacetBevels(facet_t *facet)
 				{
 					continue;
 				}
-				// see if the plane is allready present
+				// see if the plane is already present
 				for (i = 0; i < facet->numBorders; i++)
 				{
 					if (CM_PlaneEqual(&planes[facet->borderPlanes[i]], plane, &flipped))
