b5ffb5b441ea644a1dde77dfdc01cccb5674867d
Remy Marquis <remy.marquis@gmail.com>
2016-09-15 18:57:19 +0200

    cgame: hide skill rating in debriefing when disabled, refs #478 #403

--- src/cgame/cg_debriefing.c
+++ src/cgame/cg_debriefing.c
@@ -2392,12 +2392,15 @@ void CG_Debriefing_PlayerXP_Draw(panel_button_t *button)
 #ifdef FEATURE_RATING
 void CG_Debriefing_PlayerSR_Draw(panel_button_t *button)
 {
-	clientInfo_t *ci = CG_Debriefing_GetSelectedClientInfo();
-	float        w   = CG_Text_Width_Ext("SR: ", button->font->scalex, 0, button->font->font);
+	if (cgs.skillRating)
+	{
+		clientInfo_t *ci = CG_Debriefing_GetSelectedClientInfo();
+		float        w   = CG_Text_Width_Ext("SR: ", button->font->scalex, 0, button->font->font);
 
-	CG_Text_Paint_Ext(button->rect.x - w, button->rect.y, button->font->scalex, button->font->scaley, button->font->colour, CG_TranslateString("SR:"), 0, 0, ITEM_TEXTSTYLE_SHADOWED, button->font->font);
+		CG_Text_Paint_Ext(button->rect.x - w, button->rect.y, button->font->scalex, button->font->scaley, button->font->colour, CG_TranslateString("SR:"), 0, 0, ITEM_TEXTSTYLE_SHADOWED, button->font->font);
 
-	CG_Text_Paint_Ext(button->rect.x, button->rect.y, button->font->scalex, button->font->scaley, button->font->colour, va("%.1f (^5%+.1f^7)", ci->rating, ci->deltaRating), 0, 0, ITEM_TEXTSTYLE_SHADOWED, button->font->font);
+		CG_Text_Paint_Ext(button->rect.x, button->rect.y, button->font->scalex, button->font->scaley, button->font->colour, va("%.1f (^5%+.1f^9)", ci->rating, ci->deltaRating), 0, 0, ITEM_TEXTSTYLE_SHADOWED, button->font->font);
+	}
 }
 #endif
 
