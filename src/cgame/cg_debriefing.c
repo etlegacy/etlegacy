845e23302558bbf73a44f821c11aa8cd79616e6e
Remy Marquis <remy.marquis@gmail.com>
2016-09-15 21:06:20 +0200

    mod: do not send any rating data when disabled, refs #403

--- src/cgame/cg_debriefing.c
+++ src/cgame/cg_debriefing.c
@@ -1475,7 +1475,7 @@ void CG_Debriefing_InfoRequests(void)
 	}
 
 #ifdef FEATURE_RATING
-	if (!cgs.dbSkillRatingRecieved)
+	if (!cgs.dbSkillRatingRecieved && cgs.skillRating)
 	{
 		trap_SendClientCommand("imsr");
 		return;
@@ -2001,7 +2001,10 @@ qboolean CG_Debriefing_ServerCommand(const char *cmd)
 #ifdef FEATURE_RATING
 	else if (!Q_stricmp(cmd, "imsr"))
 	{
-		CG_Debriefing_ParseSkillRating();
+		if (cgs.skillRating)
+		{
+			CG_Debriefing_ParseSkillRating();
+		}
 		return qtrue;
 	}
 #endif
