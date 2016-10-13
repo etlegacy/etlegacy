a5ed523e8a51c02565e9cf26ee4e639a8fb0f203
Aranud <armarty@emea.msad.sopra>
2016-10-13 10:55:11 +0200

    qcommon: const char*

--- src/qcommon/qcommon.h
+++ src/qcommon/qcommon.h
@@ -1162,7 +1162,7 @@ void SCR_DebugGraph(float value);     // FIXME: move logging to common?
 // server interface
 
 void SV_Init(void);
-void SV_Shutdown(char *finalmsg);
+void SV_Shutdown(const char *finalmsg);
 void SV_Frame(int msec);
 void SV_PacketEvent(netadr_t from, msg_t *msg);
 qboolean SV_GameCommand(void);
