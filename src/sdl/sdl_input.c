19c0a6d3f56816dded8b88d5cb467f0b8ae019bd
IR4T4 <gutzu@gmx.ch>
2016-12-04 09:51:29 +0100

    client: Added joystick cvars to UI, cvar joy_threshold renamed to in_joystickThreshold

--- src/sdl/sdl_input.c
+++ src/sdl/sdl_input.c
@@ -1379,7 +1379,7 @@ void IN_Init(void)
 	in_nograb = Cvar_Get("in_nograb", "0", CVAR_ARCHIVE);
 
 	in_joystick          = Cvar_Get("in_joystick", "0", CVAR_ARCHIVE | CVAR_LATCH);
-	in_joystickThreshold = Cvar_Get("joy_threshold", "0.15", CVAR_ARCHIVE);
+	in_joystickThreshold = Cvar_Get("in_joystickThreshold", "0.15", CVAR_ARCHIVE);
 
 	SDL_StartTextInput();
 
