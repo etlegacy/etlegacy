0920ec28054ad3ff7f9a304d91471d2464e05d36
IR4T4 <gutzu@gmx.ch>
2017-01-27 14:22:34 +0100

    client: fix interrupting looping sounds for OpenAL

--- src/client/snd_openal.c
+++ src/client/snd_openal.c
@@ -1502,7 +1502,6 @@ static void S_AL_SrcLoop(alSrcPriority_t priority, sfxHandle_t sfx,
 	src_t     *curSource;
 	vec3_t    sorigin, svelocity;
 
-	//TODO: implement soundTime
 	//if(S_AL_CheckInput(entityNum, sfx))
 	//  return;
 
@@ -1546,9 +1545,16 @@ static void S_AL_SrcLoop(alSrcPriority_t priority, sfxHandle_t sfx,
 	sent->loopSfx      = sfx;
 	sent->volume       = volume;
 
-	// If this is not set then the looping sound is stopped.
-	sent->loopAddedThisFrame = qtrue;
-
+	if (soundTime != 0)
+	{
+		// If this is not set then the looping sound is stopped.
+		sent->loopAddedThisFrame = qtrue;
+	}
+	else
+	{
+		sent->loopAddedThisFrame = qfalse;
+	}
+	
 	// UGH
 	// These lines should be called via S_AL_SrcSetup, but we
 	// can't call that yet as it buffers sfxes that may change
