629dfc0eca57e42f633ba281423e49bb84b097ca
Remy Marquis <remy.marquis@gmail.com>
2017-02-04 12:28:52 +0100

    game: fixed comparison of unsigned enum expression warning

--- src/game/g_combat.c
+++ src/game/g_combat.c
@@ -540,7 +540,7 @@ void player_die(gentity_t *self, gentity_t *inflictor, gentity_t *attacker, int 
 	{
 		char *obit;
 
-		if (meansOfDeath < 0 || meansOfDeath >= sizeof(modNames) / sizeof(modNames[0]))
+		if (meansOfDeath >= sizeof(modNames) / sizeof(modNames[0]))
 		{
 			obit = "<bad obituary>";
 		}
