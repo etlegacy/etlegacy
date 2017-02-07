7f5c2493b392431eaed28389946eda8ae8a7cf35
IR4T4 <gutzu@gmx.ch>
2017-02-07 21:19:06 +0100

    irc: fix overflow

--- src/irc/irc_client.c
+++ src/irc/irc_client.c
@@ -1928,7 +1928,7 @@ static qboolean IRC_AddSendItem(qboolean is_action, const char *string)
 		return qfalse;
 	}
 
-	strcpy(IRC_SendQueue[IRC_SendQueue_Write].string, string);
+	Q_strncpyz(IRC_SendQueue[IRC_SendQueue_Write].string, string, IRC_MAX_SEND_LEN);
 	IRC_SendQueue[IRC_SendQueue_Write].is_action   = is_action;
 	IRC_SendQueue[IRC_SendQueue_Write].has_content = qtrue;
 	IRC_SendQueue_Write                            = (IRC_SendQueue_Write + 1) % IRC_SENDQUEUE_SIZE;
