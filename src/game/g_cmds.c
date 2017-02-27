2c6b9e64b57ff40aa3145ce0ca444fbc497f2b45
Aranud <aranud66@live.fr>
2017-02-27 13:47:19 +0100

    game: no need to check twice != '\0'

--- src/game/g_cmds.c
+++ src/game/g_cmds.c
@@ -5231,7 +5231,7 @@ void ClientCommand(int clientNum)
 }
 
 /**
- * @brief Replaces all occurances of "\\n" with '\\n'
+ * @brief Replaces all occurances of "\\\\n" with '\\n'
  *
  * @param[in] s
  */
@@ -5251,7 +5251,7 @@ char *Q_AddCR(char *s)
 			copy  = s;
 			place = s;
 			s++;
-			if (*s && *s == 'n')
+			if (*s == 'n')
 			{
 				*copy = '\n';
 				while (*++s)
