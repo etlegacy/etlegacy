974e016a3e77957ce46f02d8dcf9c6d34912df6d
Aranud <aranud66@live.fr>
2017-06-28 10:13:14 +0200

    qcommon: unused variable

--- src/qcommon/common.c
+++ src/qcommon/common.c
@@ -77,8 +77,9 @@ int demo_protocols[] =
 #define DEF_COMHUNKMEGS_S           XSTRING(DEF_COMHUNKMEGS)
 #define DEF_COMZONEMEGS_S           XSTRING(DEF_COMZONEMEGS)
 
-int  com_argc;
-char *com_argv[MAX_NUM_ARGVS + 1];
+// TODO: Unused
+// int  com_argc;
+// char *com_argv[MAX_NUM_ARGVS + 1];
 
 jmp_buf abortframe;     // an ERR_DROP occured, exit the entire frame
 
