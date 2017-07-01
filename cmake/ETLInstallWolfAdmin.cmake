c0e1d80940996731b3f5207497a1bfa37fdc2e85
Remy Marquis <remy.marquis@gmail.com>
2017-07-01 14:52:01 +0200

    misc: fixed WolfAdmin install path, refs #975

--- cmake/ETLInstallWolfAdmin.cmake
+++ cmake/ETLInstallWolfAdmin.cmake
@@ -30,7 +30,30 @@ elseif(WIN32 AND GZIP_EXECUTABLE)
 	)
 endif(UNIX)
 
-message(STATUS "Adding WolfAdmin to installer scripts")
-install(DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}/legacy/wolfadmin/"
-	DESTINATION "${INSTALL_DEFAULT_MODDIR}/legacy/wolfadmin"
+file(COPY "${CMAKE_CURRENT_BINARY_DIR}/legacy/wolfadmin/luamods"
+	DESTINATION "${CMAKE_CURRENT_BINARY_DIR}/legacy"
 )
+file(COPY "${CMAKE_CURRENT_BINARY_DIR}/legacy/wolfadmin/lualibs"
+	DESTINATION "${CMAKE_CURRENT_BINARY_DIR}/legacy"
+)
+file(GLOB ETLEGACY_WOLFADMIN_CONFIG
+	"${CMAKE_CURRENT_BINARY_DIR}/legacy/wolfadmin/config/*.toml"
+)
+foreach(ETLEGACY_WOLFADMIN_CONFIG ${ETLEGACY_WOLFADMIN_CONFIG})
+	file(COPY "${ETLEGACY_WOLFADMIN_CONFIG}"
+		DESTINATION "${CMAKE_CURRENT_BINARY_DIR}/legacy"
+	)
+endforeach()
+
+message(STATUS "Adding WolfAdmin to installer scripts")
+install(DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}/legacy/luamods"
+	DESTINATION "${INSTALL_DEFAULT_MODDIR}/legacy"
+)
+install(DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}/legacy/lualibs"
+	DESTINATION "${INSTALL_DEFAULT_MODDIR}/legacy"
+)
+foreach(ETLEGACY_WOLFADMIN_CONFIG ${ETLEGACY_WOLFADMIN_CONFIG})
+	install(FILES "${ETLEGACY_WOLFADMIN_CONFIG}"
+		DESTINATION "${INSTALL_DEFAULT_MODDIR}/legacy"
+	)
+endforeach()
