3dab680c144a9a28ec9b60a0b784980baa790f96
Aranud <aranud66@live.fr>
2017-07-03 09:27:15 +0200

    sys: recover erased security fix ... sorry !

--- src/sys/sys_main.c
+++ src/sys/sys_main.c
@@ -517,6 +517,13 @@ void *Sys_LoadDll(const char *name, qboolean useSystemLib)
 {
 	void *dllhandle;
 
+	// Don't load any DLLs that end with the pk3 extension or try to traverse directories
+	if (COM_CompareExtension(name, ".pk3") || strstr(name, "/") || strstr(name, "\\") || strstr(name, "..") || strstr(name, "::"))
+	{
+		Com_Printf("Rejecting DLL named '%s'\n", name);
+		return NULL;
+	}
+
 	if (useSystemLib)
 	{
 		Com_Printf("Trying to load \"%s\"...\n", name);
