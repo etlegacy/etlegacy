a50142033881e913b37a3e8c7de2bea3f31d249e
Jacker <shogaiam@gmail.com>
2017-07-05 00:33:24 +0300

    r2: yeah typo on the last fix

--- src/tools/shdr/shaders2h.cpp
+++ src/tools/shdr/shaders2h.cpp
@@ -192,7 +192,7 @@ static void make_c_string(string &in)
 	}
 
 	// Make sure the output ends with a new line char
-	if(out.size() > 0 && out[out.size() - 1] != '\n')
+	if(out.size() > 2 && (out[out.size() - 2] != '\\' || out[out.size() - 1] != 'n'))
 	{
 		out += "\\n";
 	}
