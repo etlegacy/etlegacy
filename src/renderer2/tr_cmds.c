02606057d39702bbc4205f350b18d8d92ef0154e
IR4T4 <gutzu@gmx.ch>
2017-11-04 20:05:40 +0100

    renderers: don't assign r_measureOverdraw->modified twice

--- src/renderer2/tr_cmds.c
+++ src/renderer2/tr_cmds.c
@@ -563,7 +563,11 @@ void RE_BeginFrame()
 		{
 			Ren_Print("Warning: not enough stencil bits to measure overdraw: %d\n", glConfig.stencilBits);
 			ri.Cvar_Set("r_measureOverdraw", "0");
-			r_measureOverdraw->modified = qfalse;
+		}
+		else if (r_shadows->integer == 2)
+		{
+			Ren_Print("Warning: stencil shadows and overdraw measurement are mutually exclusive\n");
+			ri.Cvar_Set("r_measureOverdraw", "0");
 		}
 		else
 		{
