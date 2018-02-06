4bcd3a7b55817e40fc25412cf30b45c8857a6303
twt_thunder <thunder@thewolfteam.org>
2018-02-06 20:58:18 +0100

    renderer2: Lightmap glsl shader without black dots when normalmapping is used

--- src/renderer2/glsl/lightMapping_fp.glsl
+++ src/renderer2/glsl/lightMapping_fp.glsl
@@ -25,7 +25,7 @@ varying vec3 var_Normal;
 
 varying vec4 var_Color;
 
-#define LIGTMAP_INTENSITY 0.75
+#define LIGTMAP_INTENSITY 1
 
 void main()
 {
@@ -122,26 +122,26 @@ void main()
 
 
 	// compute normal in world space from normalmap
-	vec3 N = (2.0 * (texture2D(u_NormalMap, texNormal).xyz - 0.5));
+	vec3 N = normalize(2.0 * (texture2D(u_NormalMap, texNormal).xyz - 0.5));
 	//N.x = -N.x;
 	//N = normalize(N);
 	//N = normalize(var_Normal.xyz);
-	N = normalize(tangentToWorldMatrix * N);
+	// too expensive in fragment : N = normalize(tangentToWorldMatrix * N);
 
 	// compute light direction in world space
-	vec3 L = 2.0 * (deluxemapColor.xyz - 0.5);
+	vec3 L = normalize(2.0 * (deluxemapColor.xyz - 0.5));
 	//L = normalize(L);
 
 	// compute half angle in world space
 	vec3 H = normalize(L + I);
 
-	// compute light color from world space lightmap
-	vec3 lightColor = lightmapColor.rgb;
-
 	// compute the specular term
 	vec3 specular = texture2D(u_SpecularMap, texSpecular).rgb;
 
 	float NdotL = clamp(dot(N, L), 0.0, 1.0);
+
+	// compute light color from world space lightmap
+	vec3 lightColor = lightmapColor.rgb *NdotL;
 
 	float NdotLnobump = clamp(dot(normalize(var_Normal.xyz), L), 0.004, 1.0);
 	//vec3 lightColorNoNdotL = clamp(lightColor.rgb / NdotLnobump, 0.0, 1.0);
@@ -155,10 +155,11 @@ void main()
 	//color.rgb = vec3(NdotLnobump, NdotLnobump, NdotLnobump);
 	//color.rgb *= lightColor.rgb;
 	//color.rgb = lightColorNoNdotL.rgb * NdotL;
-	color.rgb *= clamp(lightColorNoNdotL.rgb * NdotL, lightColor.rgb * 0.3, lightColor.rgb);
+	//color.rgb *= clamp(lightColorNoNdotL.rgb * NdotL, lightColor.rgb * 1.0, lightColor.rgb);
 	//color.rgb *= diffuse.rgb;
 	//color.rgb = L * 0.5 + 0.5;
-	color.rgb += specular * lightColorNoNdotL * pow(clamp(dot(N, H), 0.0, 1.0), r_SpecularExponent) * r_SpecularScale;
+	color.rgb *= lightColor;
+	color.rgb += specular * lightColor * pow(clamp(dot(N, H), 0.0, 1.0), r_SpecularExponent) * r_SpecularScale;
 	// for smooth terrain blending
 	color.a   *= var_Color.a; 
 
