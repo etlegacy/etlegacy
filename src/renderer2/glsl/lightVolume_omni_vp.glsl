671f6f19676f36a7ed2bd941dfb294e334bba391
twt_thunder <thunder@thewolfteam.org>
2018-03-10 20:01:28 +0100

    renderer2: added vertexshader

---
+++ src/renderer2/glsl/lightVolume_omni_vp.glsl
@@ -0,0 +1,11 @@
+/*  lightVolume_omni_vp.glsl  */
+
+attribute vec4		attr_Position;
+
+uniform mat4		u_ModelViewProjectionMatrix;
+
+void	main()
+{
+	// transform vertex position into homogenous clip-space
+	gl_Position = u_ModelViewProjectionMatrix * attr_Position;
+}
