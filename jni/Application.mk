08fedc2a13f6dd7a5679fa5747147f9d54dcc094
rafal1137 <rafal1137@yahoo.pl>
2018-07-09 23:30:01 +0200

    android: added jni directory including Android MakeFiles and libs

---
+++ jni/Application.mk
@@ -0,0 +1,7 @@
+
+# Uncomment this if you're using STL in your project
+# See CPLUSPLUS-SUPPORT.html in the NDK documentation for more information
+#APP_STL := stlport_static
+APP_STL := gnustl_static
+APP_ABI := armeabi-v7a
+APP_OPTIM := release
---
+++ jni/src/zlib.mk
@@ -0,0 +1,4 @@
+#Include zlib
+LOCAL_MODULE    := zlib
+LOCAL_SRC_FILES := libz.a
+include $(PREBUILT_STATIC_LIBRARY)
