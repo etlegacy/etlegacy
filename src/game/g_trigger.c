475e985b73cc0003b4d63290a48464f8cf6fbd68
IR4T4 <gutzu@gmx.ch>
2018-09-01 12:15:17 +0200

    game: add team-specific activate stages for multi_trigger fixes #1105

--- src/game/g_trigger.c
+++ src/game/g_trigger.c
@@ -111,7 +111,7 @@ void multi_trigger(gentity_t *ent, gentity_t *activator)
 		return;     // can't retrigger until the wait is over
 	}
 
-	G_Script_ScriptEvent(ent, "activate", NULL);
+	G_Script_ScriptEvent(ent, "activate", activator->client->sess.sessionTeam == TEAM_AXIS ? "axis" : "allies");
 
 	G_UseTargets(ent, ent->activator);
 
