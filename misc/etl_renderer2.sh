831d93cb4f523fee0e34348ccede6ebcf00ae702
IR4T4 <gutzu@gmx.ch>
2018-09-11 13:06:39 +0200

    misc: r2 linux start script

---
+++ misc/etl_renderer2.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# Simple script to start ET Legacy client with experimental new renderer
+#
+#./etl +set cl_renderer opengl1 +set com_hunkmegs 128 
+./etl +set cl_renderer opengl2 +set com_hunkmegs 512  
