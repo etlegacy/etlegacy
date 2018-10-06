d04deb47bab14e16cb0fef7366cb3033dadeeac9
Rafał Pawlak <rafal1137@yahoo.pl>
2018-10-07 00:07:13 +0200

    rendererGLES: added some OpenGL ES implementation, refs #1100

--- src/rendererGLES/tr_main.c
+++ src/rendererGLES/tr_main.c
@@ -1781,23 +1781,14 @@ void R_DebugPolygon(int color, int numPoints, float *points)
 
 	// draw solid shade
 	qglColor3f(color & 1, (color >> 1) & 1, (color >> 2) & 1);
-	qglBegin(GL_TRIANGLE_FAN);
-	for (i = 0 ; i < numPoints ; i++)
-	{
-		qglVertex3fv(points + i * 3);
-	}
-	qglEnd();
+	qglVertexPointer(3, GL_FLOAT, 0, points);
+	qglDrawArrays(GL_TRIANGLE_FAN, 0, numPoints);
 
 	// draw wireframe outline
-	GL_State(GLS_POLYMODE_LINE | GLS_DEPTHMASK_TRUE | GLS_SRCBLEND_ONE | GLS_DSTBLEND_ONE);
 	qglDepthRange(0, 0);
 	qglColor3f(1, 1, 1);
-	qglBegin(GL_TRIANGLE_FAN);
-	for (i = 0 ; i < numPoints ; i++)
-	{
-		qglVertex3fv(points + i * 3);
-	}
-	qglEnd();
+	qglVertexPointer(3, GL_FLOAT, 0, points);
+	qglDrawArrays(GL_LINES, 0, numPoints);
 	qglDepthRange(0, 1);
 }
 
