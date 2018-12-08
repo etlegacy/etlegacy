d673141c32c8994002dcffa180b9902924037cf3
rafal1137 <rafal1137@yahoo.pl>
2018-12-08 17:06:10 +0100

    server: disable this due to "error: "format string is not a string literal (potentially insecure) [-Werror,-Wformat-security]""

--- src/server/sv_demo.c
+++ src/server/sv_demo.c
@@ -915,7 +915,9 @@ static void SV_DemoPlaybackError(const char *message)
 
 	SV_DemoStopPlayback();
 
-	Com_Error(ERR_DROP, "%s", message);
+#ifndef __ANDROID__
+	Com_Error(ERR_DROP, message); // This throws error: "format string is not a string literal (potentially insecure) [-Werror,-Wformat-security]"
+#endif
 }
 
 /**
@@ -1891,7 +1893,9 @@ read_next_demo_event: // used to read next demo event
 			    break;
 			*/
 			case -1: // no more chars in msg FIXME: inspect!
-				Com_DPrintf("%s, %i, %i:%i", "SV_DemoReadFrame: no chars ", cmd, msg.readcount, msg.cursize);
+#ifndef __ANDROID__
+				Com_DPrintf((va("SV_DemoReadFrame: no chars [%i %i:%i]", cmd, msg.readcount, msg.cursize)));
+#endif
 				return;
 			case demo_endFrame:     // end of the frame - players and entities game status update: we commit every demo entity to the server, update the server time, then release the demo frame reading here to the next server (and demo) frame
 				Com_DPrintf(" END OF FRAME \n");
