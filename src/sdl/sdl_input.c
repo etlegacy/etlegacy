cb86a1ef33fc439f41a610fb536609ee33cc71ea
rafal1137 <rafal1137@yahoo.pl>
2019-08-03 16:51:23 +0200

    sdl: Replace SDL_HINT_ANDROID_SEPARATE_MOUSE_AND_TOUCH with SDL_HINT_MOUSE_TOUCH_EVENTS and SDL_HINT_TOUCH_MOUSE_EVENTS, it was removed in SDL 2.0.10 and replaced by those 2 hints.

--- src/sdl/sdl_input.c
+++ src/sdl/sdl_input.c
@@ -1411,7 +1411,10 @@ void IN_Init(void)
 
 #ifdef __ANDROID__
 	// This set mouse input and touch to be handled separately
-	SDL_SetHint(SDL_HINT_ANDROID_SEPARATE_MOUSE_AND_TOUCH, "1");
+	// SDL_SetHint(SDL_HINT_ANDROID_SEPARATE_MOUSE_AND_TOUCH, "1");
+	// This has been removed and replaced with those bellow in SDL 2.0.10
+	SDL_SetHint(SDL_HINT_MOUSE_TOUCH_EVENTS, "0");
+	SDL_SetHint(SDL_HINT_TOUCH_MOUSE_EVENTS, "0");
 	SDL_CaptureMouse(1);
 #endif
 
