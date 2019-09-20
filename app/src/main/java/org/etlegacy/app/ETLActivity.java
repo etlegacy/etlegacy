1a54e96ae56b293b9fb5cf1325d38e48c391961e
rafal1137 <rafal1137@yahoo.pl>
2019-09-20 15:48:13 +0200

    java: implemented new UI Design
    There is issue with Jump Button and need to think how to solve issue with Shoot Button.

--- app/src/main/java/org/etlegacy/app/ETLActivity.java
+++ app/src/main/java/org/etlegacy/app/ETLActivity.java
@@ -2,7 +2,6 @@ package org.etlegacy.app;
 
 import android.app.AlertDialog;
 import android.content.Context;
-import android.content.ContextWrapper;
 import android.content.DialogInterface;
 import android.content.res.AssetManager;
 import android.graphics.Bitmap;
@@ -11,11 +10,10 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.RelativeLayout;
-import android.widget.Toast;
 
 import com.erz.joysticklibrary.JoyStick;
 import com.erz.joysticklibrary.JoyStick.JoyStickListener;
@@ -29,12 +27,9 @@ import java.io.InputStream;
 public class ETLActivity extends SDLActivity implements JoyStickListener
 {
     private String dir_etl;
+    private static Context context;
 
     private boolean checkGameFiles() {
-
-        ContextWrapper c = new ContextWrapper(this);
-        dir_etl = c.getFilesDir().getPath();
-        //Toast.makeText(this, c.getFilesDir().getPath(), Toast.LENGTH_LONG).show();
 
         File etmain_dir = new File(dir_etl.concat("etmain"));
         if(etmain_dir.exists() == false)
@@ -101,8 +96,9 @@ public class ETLActivity extends SDLActivity implements JoyStickListener
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Button btn = new Button(getApplicationContext());
-        btn.setText("Keyboard");
+        ImageButton btn = new ImageButton(getApplicationContext());
+        btn.setImageBitmap(getBitmapFromAsset("btn_keyboard.png"));
+        btn.setBackgroundResource(0);
         btn.setId(1);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -112,28 +108,17 @@ public class ETLActivity extends SDLActivity implements JoyStickListener
             }
         });
 
-        mLayout.addView(btn);
+        RelativeLayout.LayoutParams keyboard_layout = new RelativeLayout.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT);
 
-        ImageButton btn2 = new ImageButton(getApplicationContext());
-        btn2.setImageBitmap(getBitmapFromAsset("btn_keyboard.png"));
-        btn2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                SDLActivity.onNativeKeyDown(68);
-                SDLActivity.onNativeKeyUp(68);
-            }
-        });
+        keyboard_layout.leftMargin = 210;
+        keyboard_layout.topMargin = -20;
+        mLayout.addView(btn, keyboard_layout);
 
-        RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(
-                RelativeLayout.LayoutParams.WRAP_CONTENT,
-                RelativeLayout.LayoutParams.WRAP_CONTENT);
-
-        lp.addRule(RelativeLayout.ALIGN_PARENT_RIGHT, btn.getId());
-
-        mLayout.addView(btn2, lp);
-
-        Button esc_btn = new Button(getApplicationContext());
-        esc_btn.setText("ESC");
+        ImageButton esc_btn = new ImageButton(getApplicationContext());
+        esc_btn.setImageBitmap(getBitmapFromAsset("btn_esc.png"));
+        esc_btn.setBackgroundResource(0);
         esc_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -142,18 +127,134 @@ public class ETLActivity extends SDLActivity implements JoyStickListener
             }
         });
 
-        RelativeLayout.LayoutParams lp2 = new RelativeLayout.LayoutParams(100, 95);
+        RelativeLayout.LayoutParams lp2 = new RelativeLayout.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT);
 
         lp2.addRule(RelativeLayout.RIGHT_OF, btn.getId());
+        lp2.leftMargin = -15;
+        lp2.topMargin = -20;
 
         mLayout.addView(esc_btn, lp2);
 
-        JoyStick joyStick = new JoyStick(getApplicationContext());
+        ImageButton btn2 = new ImageButton(getApplicationContext());
+        btn2.setId(2);
+        btn2.setImageBitmap(getBitmapFromAsset("btn_sht.png"));
+        btn2.setBackgroundResource(0);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SDLActivity.onNativeKeyDown(42);
+                SDLActivity.onNativeKeyUp(42);
+            }
+        });
 
-        joyStick.setListener(this);
-        joyStick.setPadBackground(getBitmapFromAsset("btn_joystick.png"));
-        joyStick.setButtonColor(Color.RED);
-        joyStick.setButtonRadiusScale(50);
+        RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(
+                150,
+                150);
+
+        lp.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+        lp.addRule(RelativeLayout.CENTER_VERTICAL);
+        lp.rightMargin = 200;
+
+        mLayout.addView(btn2, lp);
+
+
+        ImageButton btn_reload = new ImageButton(getApplicationContext());
+        btn_reload.setImageBitmap(getBitmapFromAsset("btn_reload.png"));
+        btn_reload.setBackgroundResource(0);
+        btn_reload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SDLActivity.onNativeKeyDown(46);
+                SDLActivity.onNativeKeyUp(46);
+            }
+        });
+
+        RelativeLayout.LayoutParams lp_reload = new RelativeLayout.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT);
+
+        lp_reload.addRule(RelativeLayout.BELOW, btn2.getId());
+        lp_reload.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+        lp_reload.addRule(RelativeLayout.CENTER_VERTICAL);
+        lp_reload.rightMargin = 90;
+
+        mLayout.addView(btn_reload, lp_reload);
+
+
+        ImageButton btn_jump = new ImageButton(getApplicationContext());
+        btn_jump.setImageBitmap(getBitmapFromAsset("btn_jump.png"));
+        btn_jump.setBackgroundResource(0);
+        btn_jump.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SDLActivity.onNativeKeyUp(62);
+                SDLActivity.onNativeKeyDown(62);
+            }
+        });
+
+        RelativeLayout.LayoutParams lp_jump = new RelativeLayout.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT);
+
+        lp_jump.addRule(RelativeLayout.CENTER_HORIZONTAL);
+        lp_jump.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
+        lp_jump.bottomMargin = -20;
+
+        mLayout.addView(btn_jump, lp_jump);
+
+        ImageButton btn_activate = new ImageButton(getApplicationContext());
+        btn_activate.setImageBitmap(getBitmapFromAsset("btn_activate.png"));
+        btn_activate.setBackgroundResource(0);
+        btn_activate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SDLActivity.onNativeKeyDown(34);
+                SDLActivity.onNativeKeyUp(34);
+            }
+        });
+
+        RelativeLayout.LayoutParams lp_activate = new RelativeLayout.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT);
+
+        lp_activate.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
+        lp_activate.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
+        lp_activate.bottomMargin = -20;
+        lp_activate.leftMargin = 400;
+
+        mLayout.addView(btn_activate, lp_activate);
+
+        ImageButton btn_alternative = new ImageButton(getApplicationContext());
+        btn_alternative.setImageBitmap(getBitmapFromAsset("btn_altfire.png"));
+        btn_alternative.setBackgroundResource(0);
+        btn_alternative.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SDLActivity.onNativeKeyDown(36);
+                SDLActivity.onNativeKeyUp(36);
+            }
+        });
+
+        RelativeLayout.LayoutParams lp_alternative = new RelativeLayout.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT);
+
+        lp_alternative.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+        lp_alternative.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
+        lp_alternative.bottomMargin = -20;
+        lp_alternative.rightMargin = 400;
+
+        mLayout.addView(btn_alternative, lp_alternative);
+
+
+        JoyStick joyStick_left = new JoyStick(getApplicationContext());
+
+        joyStick_left.setListener(this);
+        joyStick_left.setPadColor(Color.TRANSPARENT);
+        joyStick_left.setButtonColor(Color.TRANSPARENT);
+        joyStick_left.setButtonRadiusScale(50);
 
         RelativeLayout.LayoutParams joystick_layout = new RelativeLayout.LayoutParams(
                 400,
@@ -161,17 +262,16 @@ public class ETLActivity extends SDLActivity implements JoyStickListener
 
         joystick_layout.addRule(RelativeLayout.ALIGN_LEFT);
         joystick_layout.addRule(RelativeLayout.CENTER_VERTICAL);
-        joystick_layout.leftMargin =     10;
+        joystick_layout.leftMargin = 10;
 
-        mLayout.addView(joyStick, joystick_layout);
+        mLayout.addView(joyStick_left, joystick_layout);
 
     }
 
     @Override
     public void onMove(JoyStick joyStick, double angle, double power, int direction) {
 
-        switch(direction)
-        {
+        switch (direction) {
             case JoyStick.DIRECTION_CENTER:
                 SDLActivity.onNativeKeyUp(51);
                 SDLActivity.onNativeKeyUp(32);
