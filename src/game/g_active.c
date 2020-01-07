fd755973899e8ec525e4e1f1ff2eefe8de7dd1ec
Remy Marquis <remy.marquis@gmail.com>
2020-01-07 12:14:30 +0100

    game: fixed time played stats reset in 2nd round of stopwatch, fixes #1381

--- src/game/g_active.c
+++ src/game/g_active.c
@@ -2106,7 +2106,8 @@ void ClientEndFrame(gentity_t *ent)
 	{
 		// ensure time played is always smaller or equal than time spent in teams
 		// work around for unreset data of slow connecters
-		if (ent->client->sess.time_played > (ent->client->sess.time_axis + ent->client->sess.time_allies))
+		if ((ent->client->sess.time_played > (ent->client->sess.time_axis + ent->client->sess.time_allies)) &&
+		    !(g_gametype.integer == GT_WOLF_STOPWATCH && g_currentRound.integer == 1))
 		{
 			ent->client->sess.time_played = 0;
 		}
