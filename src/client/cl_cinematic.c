33f1610f9ecc827745785c374d49dce7505fdaee
Jacker <shogaiam@gmail.com>
2020-02-14 18:15:19 +0200

    client: do not crash the client if the video frame fails to decode, refs #1397

--- src/client/cl_cinematic.c
+++ src/client/cl_cinematic.c
@@ -715,7 +715,7 @@ void CIN_UploadCinematic(int handle)
 
 	cin = CIN_GetCinematicByHandle(handle);
 
-	if (!cin->currentData.image)
+	if (!cin->currentData.image || !cin->currentData.image[0])
 	{
 		return;
 	}
