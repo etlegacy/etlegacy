f452096560f1d2f951e9d109901cfe2e00a0df8b
Remy Marquis <remy.marquis@gmail.com>
2020-03-28 12:09:46 +0100

    sdl: fixed condition to avoid logically dead code, refs #1409
    Also slightly increased fallback resolution

--- src/sdl/sdl_glimp.c
+++ src/sdl/sdl_glimp.c
@@ -223,7 +223,7 @@ static void GLimp_InitCvars(void)
 	r_customaspect   = Cvar_Get("r_customaspect", "1", CVAR_ARCHIVE | CVAR_LATCH);
 	r_displayRefresh = Cvar_Get("r_displayRefresh", "0", CVAR_LATCH);
 	Cvar_CheckRange(r_displayRefresh, 0, 240, qtrue);
-    r_windowLocation = Cvar_Get("r_windowLocation", "0,-1,-1", CVAR_ARCHIVE | CVAR_PROTECTED);
+	r_windowLocation = Cvar_Get("r_windowLocation", "0,-1,-1", CVAR_ARCHIVE | CVAR_PROTECTED);
 
 	// Window render surface cvars
 	r_stencilbits     = Cvar_Get("r_stencilbits", "0", CVAR_ARCHIVE | CVAR_LATCH | CVAR_UNSAFE);
@@ -256,10 +256,10 @@ void GLimp_Shutdown(void)
 
 	if (main_window)
 	{
-	    int tmpX = SDL_WINDOWPOS_UNDEFINED, tmpY = SDL_WINDOWPOS_UNDEFINED;
-	    int displayIndex = SDL_GetWindowDisplayIndex(main_window);
-        SDL_GetWindowPosition(main_window, &tmpX, &tmpY);
-        Cvar_Set("r_windowLocation", va("%d,%d,%d", displayIndex, tmpX, tmpY));
+		int tmpX = SDL_WINDOWPOS_UNDEFINED, tmpY = SDL_WINDOWPOS_UNDEFINED;
+		int displayIndex = SDL_GetWindowDisplayIndex(main_window);
+		SDL_GetWindowPosition(main_window, &tmpX, &tmpY);
+		Cvar_Set("r_windowLocation", va("%d,%d,%d", displayIndex, tmpX, tmpY));
 
 		SDL_DestroyWindow(main_window);
 		main_window = NULL;
@@ -410,99 +410,100 @@ static void GLimp_DetectAvailableModes(void)
  * @param y[in,out] Y location
  * @param fullscreen requested to run fullscreen
  */
-static void GLimp_WindowLocation(glconfig_t *glConfig, int *x, int *y, const qboolean fullscreen) {
-    int displayIndex = 0, tmpX = SDL_WINDOWPOS_UNDEFINED, tmpY = SDL_WINDOWPOS_UNDEFINED;
-    int numDisplays = SDL_GetNumVideoDisplays();
+static void GLimp_WindowLocation(glconfig_t *glConfig, int *x, int *y, const qboolean fullscreen)
+{
+	int displayIndex = 0, tmpX = SDL_WINDOWPOS_UNDEFINED, tmpY = SDL_WINDOWPOS_UNDEFINED;
+	int numDisplays = SDL_GetNumVideoDisplays();
 
-    if(!r_windowLocation->string || !r_windowLocation->string[0])
-    {
-        // Center window
-        if (r_centerWindow->integer && !fullscreen)
-        {
-            *x = SDL_WINDOWPOS_CENTERED;
-            *y = SDL_WINDOWPOS_CENTERED;
-        }
-        else
-        {
-            *x = SDL_WINDOWPOS_UNDEFINED;
-            *y = SDL_WINDOWPOS_UNDEFINED;
-        }
-        return;
-    }
+	if (!r_windowLocation->string || !r_windowLocation->string[0])
+	{
+		// Center window
+		if (r_centerWindow->integer && !fullscreen)
+		{
+			*x = SDL_WINDOWPOS_CENTERED;
+			*y = SDL_WINDOWPOS_CENTERED;
+		}
+		else
+		{
+			*x = SDL_WINDOWPOS_UNDEFINED;
+			*y = SDL_WINDOWPOS_UNDEFINED;
+		}
+		return;
+	}
 
-    // We might be in headless mode, just ignore for now (xD)
-    if(numDisplays < 0)
-    {
-        numDisplays = 1;
-    }
+	// We might be in headless mode, just ignore for now (xD)
+	if (numDisplays < 0)
+	{
+		numDisplays = 1;
+	}
 
-    if(sscanf(r_windowLocation->string, "%d,%d,%d", &displayIndex, &tmpX, &tmpY) != 3)
-    {
-        return;
-    }
+	if (sscanf(r_windowLocation->string, "%d,%d,%d", &displayIndex, &tmpX, &tmpY) != 3)
+	{
+		return;
+	}
 
-    if(displayIndex < 0 && displayIndex >= numDisplays)
-    {
-        // Center window
-        if (r_centerWindow->integer && !fullscreen)
-        {
-            *x = SDL_WINDOWPOS_CENTERED;
-            *y = SDL_WINDOWPOS_CENTERED;
-        }
-        else
-        {
-            *x = SDL_WINDOWPOS_UNDEFINED;
-            *y = SDL_WINDOWPOS_UNDEFINED;
-        }
-        return;
-    }
+	if (displayIndex < 0 || displayIndex >= numDisplays)
+	{
+		// Center window
+		if (r_centerWindow->integer && !fullscreen)
+		{
+			*x = SDL_WINDOWPOS_CENTERED;
+			*y = SDL_WINDOWPOS_CENTERED;
+		}
+		else
+		{
+			*x = SDL_WINDOWPOS_UNDEFINED;
+			*y = SDL_WINDOWPOS_UNDEFINED;
+		}
+		return;
+	}
 
-    // Center window
-    if (r_centerWindow->integer && !fullscreen)
-    {
-        *x = SDL_WINDOWPOS_CENTERED_DISPLAY(displayIndex);
-        *y = SDL_WINDOWPOS_CENTERED_DISPLAY(displayIndex);
-        return;
-    }
+	// Center window
+	if (r_centerWindow->integer && !fullscreen)
+	{
+		*x = SDL_WINDOWPOS_CENTERED_DISPLAY(displayIndex);
+		*y = SDL_WINDOWPOS_CENTERED_DISPLAY(displayIndex);
+		return;
+	}
 
-    if (fullscreen || r_mode->integer == -2)
-    {
-        *x = SDL_WINDOWPOS_UNDEFINED_DISPLAY(displayIndex);
-        *y = SDL_WINDOWPOS_UNDEFINED_DISPLAY(displayIndex);
-        return;
-    }
+	if (fullscreen || r_mode->integer == -2)
+	{
+		*x = SDL_WINDOWPOS_UNDEFINED_DISPLAY(displayIndex);
+		*y = SDL_WINDOWPOS_UNDEFINED_DISPLAY(displayIndex);
+		return;
+	}
 
-    // Default values so we skip out with safe values
-    if(tmpX == -1 && tmpY == -1)
-    {
-        *x = SDL_WINDOWPOS_UNDEFINED;
-        *y = SDL_WINDOWPOS_UNDEFINED;
-        return;
-    }
+	// Default values so we skip out with safe values
+	if (tmpX == -1 && tmpY == -1)
+	{
+		*x = SDL_WINDOWPOS_UNDEFINED;
+		*y = SDL_WINDOWPOS_UNDEFINED;
+		return;
+	}
 
-    SDL_Rect rect;
-    SDL_GetDisplayBounds(displayIndex, &rect);
+	SDL_Rect rect;
+	SDL_GetDisplayBounds(displayIndex, &rect);
 
-    // SDL resets the values to displays origins when switching between windowed and fullscreen, so just move it a bit
-    if(tmpX == rect.x && tmpY == rect.y)
-    {
-        *x = SDL_WINDOWPOS_UNDEFINED_DISPLAY(displayIndex);
-        *y = SDL_WINDOWPOS_UNDEFINED_DISPLAY(displayIndex);
-        return;
-    }
+	// SDL resets the values to displays origins when switching between windowed and fullscreen, so just move it a bit
+	if (tmpX == rect.x && tmpY == rect.y)
+	{
+		*x = SDL_WINDOWPOS_UNDEFINED_DISPLAY(displayIndex);
+		*y = SDL_WINDOWPOS_UNDEFINED_DISPLAY(displayIndex);
+		return;
+	}
 
-    // Make sure we have at least half of the game screen visible on the display its supposed to be in
-    if ((tmpX + (glConfig->vidWidth / 2)) > rect.x && (tmpX + (glConfig->vidWidth / 2)) < (rect.x + rect.w)
-    && (tmpY + (glConfig->vidHeight / 2)) > rect.y && (tmpY + (glConfig->vidHeight / 2)) < (rect.y + rect.h))
-    {
-        *x = tmpX;
-        *y = tmpY;
+	// Make sure we have at least half of the game screen visible on the display its supposed to be in
+	if ((tmpX + (glConfig->vidWidth / 2)) > rect.x && (tmpX + (glConfig->vidWidth / 2)) < (rect.x + rect.w)
+	    && (tmpY + (glConfig->vidHeight / 2)) > rect.y && (tmpY + (glConfig->vidHeight / 2)) < (rect.y + rect.h))
+	{
+		*x = tmpX;
+		*y = tmpY;
 
-        return;
-    }
+		return;
+	}
 
-    *x = SDL_WINDOWPOS_UNDEFINED_DISPLAY(displayIndex);
-    *y = SDL_WINDOWPOS_UNDEFINED_DISPLAY(displayIndex);
+	*x = SDL_WINDOWPOS_UNDEFINED_DISPLAY(displayIndex);
+	*y = SDL_WINDOWPOS_UNDEFINED_DISPLAY(displayIndex);
 }
 
 /**
@@ -523,7 +524,7 @@ static int GLimp_SetMode(glconfig_t *glConfig, int mode, qboolean fullscreen, qb
 	SDL_Surface     *icon = NULL;
 	SDL_DisplayMode desktopMode;
 	int             display = 0;
-	int             x       = SDL_WINDOWPOS_UNDEFINED, y = SDL_WINDOWPOS_UNDEFINED;
+	int             x = SDL_WINDOWPOS_UNDEFINED, y = SDL_WINDOWPOS_UNDEFINED;
 
 	Uint32 flags = SDL_WINDOW_OPENGL | SDL_WINDOW_INPUT_GRABBED;
 
@@ -535,17 +536,17 @@ static int GLimp_SetMode(glconfig_t *glConfig, int mode, qboolean fullscreen, qb
 	}
 
 	icon = SDL_CreateRGBSurfaceFrom(
-	    (void *)CLIENT_WINDOW_ICON.pixel_data,
-	    CLIENT_WINDOW_ICON.width,
-	    CLIENT_WINDOW_ICON.height,
-	    CLIENT_WINDOW_ICON.bytes_per_pixel * 8,
-	    CLIENT_WINDOW_ICON.bytes_per_pixel * CLIENT_WINDOW_ICON.width,
+		(void *)CLIENT_WINDOW_ICON.pixel_data,
+		CLIENT_WINDOW_ICON.width,
+		CLIENT_WINDOW_ICON.height,
+		CLIENT_WINDOW_ICON.bytes_per_pixel * 8,
+		CLIENT_WINDOW_ICON.bytes_per_pixel * CLIENT_WINDOW_ICON.width,
 #ifdef Q3_LITTLE_ENDIAN
-	    0x000000FF, 0x0000FF00, 0x00FF0000, 0xFF000000
+		0x000000FF, 0x0000FF00, 0x00FF0000, 0xFF000000
 #else
-	    0xFF000000, 0x00FF0000, 0x0000FF00, 0x000000FF
+		0xFF000000, 0x00FF0000, 0x0000FF00, 0x000000FF
 #endif
-	    );
+		);
 
 	// If a window exists, note its display index
 	if (main_window != NULL)
@@ -592,7 +593,7 @@ static int GLimp_SetMode(glconfig_t *glConfig, int mode, qboolean fullscreen, qb
 	}
 	Com_Printf("%dx%d\n", glConfig->vidWidth, glConfig->vidHeight);
 
-    GLimp_WindowLocation(glConfig, &x, &y, fullscreen);
+	GLimp_WindowLocation(glConfig, &x, &y, fullscreen);
 
 	// Destroy existing state if it exists
 	if (SDL_glContext != NULL)
@@ -897,7 +898,7 @@ static qboolean GLimp_StartDriverAndSetMode(glconfig_t *glConfig, int mode, qboo
 	{
 		if (SDL_Init(SDL_INIT_VIDEO) < 0)
 		{
-			Com_Printf("SDL_Init( SDL_INIT_VIDEO ) FAILED (%s)\n", SDL_GetError());
+			Com_Printf("SDL_Init(SDL_INIT_VIDEO) FAILED (%s)\n", SDL_GetError());
 			return qfalse;
 		}
 
@@ -934,7 +935,7 @@ static qboolean GLimp_StartDriverAndSetMode(glconfig_t *glConfig, int mode, qboo
 	return qfalse;
 }
 
-#define R_MODE_FALLBACK 3 // 640 * 480
+#define R_MODE_FALLBACK 4 // 800 * 600
 
 /**
  * @brief GLimp_Splash
@@ -954,17 +955,17 @@ void GLimp_Splash(glconfig_t *glConfig)
 
 	// get splash image
 	splashImage = SDL_CreateRGBSurfaceFrom(
-			(void *)splashData,
-			CLIENT_WINDOW_SPLASH.width,
-			CLIENT_WINDOW_SPLASH.height,
-			CLIENT_WINDOW_SPLASH.bytes_per_pixel * 8,
-			CLIENT_WINDOW_SPLASH.bytes_per_pixel * CLIENT_WINDOW_SPLASH.width,
+		(void *)splashData,
+		CLIENT_WINDOW_SPLASH.width,
+		CLIENT_WINDOW_SPLASH.height,
+		CLIENT_WINDOW_SPLASH.bytes_per_pixel * 8,
+		CLIENT_WINDOW_SPLASH.bytes_per_pixel * CLIENT_WINDOW_SPLASH.width,
 #ifdef Q3_LITTLE_ENDIAN
-			0x000000FF, 0x0000FF00, 0x00FF0000, 0xFF000000
+		0x000000FF, 0x0000FF00, 0x00FF0000, 0xFF000000
 #else
-			0xFF000000, 0x00FF0000, 0x0000FF00, 0x000000FF
+		0xFF000000, 0x00FF0000, 0x0000FF00, 0x000000FF
 #endif
-	);
+		);
 
 	SDL_Rect dstRect;
 	dstRect.x = glConfig->vidWidth / 2 - splashImage->w / 2;
