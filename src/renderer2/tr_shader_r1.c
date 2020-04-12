ab71297de882021cc896a2ba3a379abf91bccd87
Dmitry Tsarevich <dimhotepus@gmail.com>
2020-04-12 16:49:44 +0300

    general: fix memset - zero full buffer

--- src/renderer2/tr_shader_r1.c
+++ src/renderer2/tr_shader_r1.c
@@ -950,8 +950,8 @@ int ScanAndLoadShaderFilesR1()
 	char         filename[MAX_QPATH];
 	long         sum = 0, summand;
 
-	Com_Memset(buffers, 0, MAX_SHADER_FILES);
-	Com_Memset(shaderTextHashTableSizes, 0, MAX_SHADER_FILES);
+	Com_Memset(buffers, 0, sizeof(buffers));
+	Com_Memset(shaderTextHashTableSizes, 0, sizeof(shaderTextHashTableSizes));
 
 	// scan for shader files
 	shaderFiles = ri.FS_ListFiles("scripts", ".shader", &numShaderFiles);
