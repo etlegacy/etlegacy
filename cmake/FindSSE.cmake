82b723a8309cbfede43b55ee07f011e169ed5c3c
Remy Marquis <remy.marquis@gmail.com>
2020-04-28 00:00:41 +0200

    general: disable SSE for debug mode, refs #1440
    Also
    * ensure -ffast-math is still set when no SSE is found (release only)
    * some format cleanup

--- cmake/FindSSE.cmake
+++ cmake/FindSSE.cmake
@@ -1,47 +1,52 @@
 # check for SSE extensions
 include(CheckCXXSourceRuns)
-if( CMAKE_COMPILER_IS_GNUCC OR CMAKE_COMPILER_IS_GNUCXX )
-    set(SSE_FLAGS)
+if(CMAKE_COMPILER_IS_GNUCC OR CMAKE_COMPILER_IS_GNUCXX)
+	set(SSE_FLAGS)
 
-    set(CMAKE_REQUIRED_FLAGS "-msse3")
-    check_cxx_source_runs("
-   #include <pmmintrin.h>
+	set(CMAKE_REQUIRED_FLAGS "-msse3")
+	check_cxx_source_runs("
+	#include <pmmintrin.h>
 
-   int main()
-   {
-      __m128d a, b;
-      double vals[2] = {0};
-      a = _mm_loadu_pd(vals);
-      b = _mm_hadd_pd(a,a);
-      _mm_storeu_pd(vals, b);
-      return 0;
-   }"
-            HAS_SSE3_EXTENSIONS)
+	int main()
+	{
+		__m128d a, b;
+		double vals[2] = {0};
+		a = _mm_loadu_pd(vals);
+		b = _mm_hadd_pd(a,a);
+		_mm_storeu_pd(vals, b);
+		return 0;
+	}"
+	HAS_SSE3_EXTENSIONS)
 
-    set(CMAKE_REQUIRED_FLAGS)
+	set(CMAKE_REQUIRED_FLAGS)
 
-    if(HAS_SSE3_EXTENSIONS)
-        message(STATUS "Using SSE3 extensions")
-        set(SSE_FLAGS "-msse3 -mfpmath=sse")
-    endif()
+	if(HAS_SSE3_EXTENSIONS)
+		message(STATUS "Using SSE3 extensions")
+		set(SSE_FLAGS "-msse3 -mfpmath=sse")
+	else()
+		set(SSE_FLAGS "-ffast-math") # no SSE flags
+	endif()
 
-    add_definitions(${SSE_FLAGS})
+	add_definitions(${SSE_FLAGS})
 elseif(MSVC)
-    check_cxx_source_runs("
-   #include <emmintrin.h>
+	check_cxx_source_runs("
+	#include <emmintrin.h>
 
-   int main()
-   {
-       __m128d a, b;
-       double vals[2] = {0};
-       a = _mm_loadu_pd(vals);
-       b = _mm_add_pd(a,a);
-       _mm_storeu_pd(vals,b);
-       return 0;
-    }"
-            HAS_SSE2_EXTENSIONS)
-    if( HAS_SSE2_EXTENSIONS )
-        message(STATUS "Using SSE2 extensions")
-        add_definitions( "/arch:SSE2 /fp:fast -D__SSE__ -D__SSE2__" )
-    endif()
+	int main()
+	{
+		__m128d a, b;
+		double vals[2] = {0};
+		a = _mm_loadu_pd(vals);
+		b = _mm_add_pd(a,a);
+		_mm_storeu_pd(vals,b);
+		return 0;
+	}"
+	HAS_SSE2_EXTENSIONS)
+
+	if(HAS_SSE2_EXTENSIONS)
+		message(STATUS "Using SSE2 extensions")
+		add_definitions("/arch:SSE2 /fp:fast -D__SSE__ -D__SSE2__")
+	else()
+		set(SSE_FLAGS "-ffast-math") # no SSE flags
+	endif()
 endif()
