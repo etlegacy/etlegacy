1de03963de61c4a9c9a3010f7ac71c5e3111628c
Michal Mitter <mittermichal@gmail.com>
2021-02-06 19:19:09 +0100

    cgame: fix timerReset/resetTimer
    same as in SHA: ce3b942c110b6ffdd240a810329a0009bf77ad38

--- src/cgame/cg_consolecmds.c
+++ src/cgame/cg_consolecmds.c
@@ -1274,16 +1274,13 @@ static void CG_TimerSet_f(void)
  */
 static void CG_TimerReset_f(void)
 {
-	int msec;
-
 	if (cgs.gamestate != GS_PLAYING)
 	{
 		CG_Printf("You may only use this command during the match.\n");
 		return;
 	}
 
-	msec = (int)(cgs.timelimit * 60000.f) - (cg.time - cgs.levelStartTime); // 60.f * 1000.f
-	trap_Cvar_Set("cg_spawnTimer_set", va("%d", msec / 1000));
+	trap_Cvar_Set("cg_spawnTimer_set", va("%d", cg.time - cgs.levelStartTime));
 }
 
 /**
