64c297be7ef2eaaacbe4698ed56266cf21568957
Michal Mitter <mittermichal@gmail.com>
2021-02-06 19:37:41 +0100

    cgame: /timerSet 0 - will disable custom spawnTimer

--- src/cgame/cg_consolecmds.c
+++ src/cgame/cg_consolecmds.c
@@ -1251,7 +1251,7 @@ static void CG_TimerSet_f(void)
 
 		if (spawnPeriod == 0)
 		{
-			trap_Cvar_Set("cg_spawnTimer_set", "-1");
+			trap_Cvar_Set("cg_spawnTimer_period", 0);
 		}
 		else if (spawnPeriod < 1 || spawnPeriod > 60)
 		{
