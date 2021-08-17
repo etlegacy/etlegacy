8199f73af7874512218712b0ad34417d4b55ccba
Sebastian <sebastian.danielsson@protonmail.com>
2021-08-17 21:23:11 +0200

    Update dl-pak0.sh

--- misc/dl-pak0.sh
+++ misc/dl-pak0.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cd "$(dirname "$0")"
 curl https://mirror.etlegacy.com/etmain/pak0.pk3 -o etmain/pak0.pk3
-curl https://mirror.etlegacy.com/etmain/pak0.pk3 -o etmain/pak1.pk3
-curl https://mirror.etlegacy.com/etmain/pak0.pk3 -o etmain/pak2.pk3
+curl https://mirror.etlegacy.com/etmain/pak1.pk3 -o etmain/pak1.pk3
+curl https://mirror.etlegacy.com/etmain/pak2.pk3 -o etmain/pak2.pk3
