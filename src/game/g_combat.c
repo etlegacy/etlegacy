c5ebe7f5e379ee68aa720a658b98dd22e8ebcb55
ryzyk_krzysiek <Frantisek.brv@gmail.com>
2021-09-15 14:18:28 +0200

    game: fix dynamite chaining deleting non armed dynamites, missed 1 check

--- src/game/g_combat.c
+++ src/game/g_combat.c
@@ -2251,7 +2251,7 @@ qboolean etpro_RadiusDamage(vec3_t origin, gentity_t *inflictor, gentity_t *atta
 		{
 			G_DPrintf("dyno chaining: inflictor: %p, ent: %p\n", inflictor->onobjective, ent->onobjective);
 
-			if (inflictor->onobjective == ent->onobjective)
+			if (inflictor->onobjective == ent->onobjective && ent->s.effect1Time)
 			{
 				if (g_dynamiteChaining.integer & DYNAMITECHAINING_FREE)
 				{
