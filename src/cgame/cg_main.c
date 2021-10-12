dd38da87cc847762fe22cc4116617607fb0db17c
Aciz <arto.karkulahti@gmail.com>
2021-10-12 13:03:48 +0300

    cgame: allow selectively disabling weapon animations (#1770)
    * cgame: allow selectively disabling weapon animations
    
    * cgame: fix cvar case

--- src/cgame/cg_main.c
+++ src/cgame/cg_main.c
@@ -161,6 +161,7 @@ vmCvar_t cg_markTime;
 vmCvar_t cg_brassTime;
 vmCvar_t cg_letterbox;
 vmCvar_t cg_drawGun;
+vmCvar_t cg_weapAnims;
 vmCvar_t cg_cursorHints;
 vmCvar_t cg_gun_frame;
 vmCvar_t cg_gun_x;
@@ -387,6 +388,7 @@ static cvarTable_t cvarTable[] =
 {
 	{ &cg_autoswitch,             "cg_autoswitch",             "2",           CVAR_ARCHIVE,                 0 },
 	{ &cg_drawGun,                "cg_drawGun",                "1",           CVAR_ARCHIVE,                 0 },
+	{ &cg_weapAnims,              "cg_weapAnims",              "15",          CVAR_ARCHIVE,                 0 },
 	{ &cg_gun_frame,              "cg_gun_frame",              "0",           CVAR_TEMP,                    0 },
 	{ &cg_cursorHints,            "cg_cursorHints",            "1",           CVAR_ARCHIVE,                 0 },
 	{ &cg_zoomDefaultSniper,      "cg_zoomDefaultSniper",      "20",          CVAR_ARCHIVE,                 0 },   // changed per atvi req
