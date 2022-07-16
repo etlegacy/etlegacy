5d7bc7d36b10bc10b1f9a5077770730ea432b914
rafal1137 <rafal1137@yahoo.pl>
2022-07-16 21:09:30 +0200

    java: try to run app in full screen over camera cutoff, refs #2039

--- app/src/main/java/org/etlegacy/app/ETLActivity.java
+++ app/src/main/java/org/etlegacy/app/ETLActivity.java
@@ -301,7 +301,13 @@ public class ETLActivity extends SDLActivity implements JoyStickListener {
     protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
 
-        setWindowStyle(true);
+		mLayout.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+				| View.SYSTEM_UI_FLAG_FULLSCREEN
+				| View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+				| View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+				| View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+				| View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+
         clipboardGetText();
 
         if(Build.VERSION.SDK_INT > Build.VERSION_CODES.M)
