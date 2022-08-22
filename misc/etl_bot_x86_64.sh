1daf5128161e26b5c4835a381d432fe16188da6c
Aciz <arto.karkulahti@gmail.com>
2022-08-22 17:46:59 +0300

    general: fix various scripts to respect new linux binary extensions, refs #2042
    * general: fix various scripts to respect new linux binary extensions
    * misc: follow systemd naming convention for services

---
+++ misc/etl_bot_x86_64.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# Simple script to start 64-bit ET: Legacy client/listen server with Omni-Bots
+#
+./etl.x86_64 +set omnibot_enable 1 +set omnibot_path "./legacy/omni-bot"
--- misc/etlded_bot.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-# Simple script to start ET: Legacy dedicated server with Omni-Bots
-#
-./etlded +set g_protect 1 +set omnibot_enable 1 +set omnibot_path "./legacy/omni-bot" +exec etl_server.cfg 
