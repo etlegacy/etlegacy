3cf9285a1128d2e8f4e436b26e849c9d47a86da9
Aranud <aranud66@live.fr>
2023-01-25 10:46:30 +0100

    cgame : fix bar vert border offset contain, refs #1967

--- src/cgame/cg_drawtools.c
+++ src/cgame/cg_drawtools.c
@@ -237,7 +237,7 @@ void CG_FilledBar(float x, float y, float w, float h, float *startColor, float *
 		CG_DrawRect_FixedBorder(x, y, w, h, indent, bgColor);
 		x += indent;
 		y += indent;
-		w -= indent;
+		w -= (2 * indent);
 		h -= (2 * indent);
 	}
 
