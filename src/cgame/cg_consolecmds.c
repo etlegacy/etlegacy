70ebbf9523cae4c99af10ff0f684560bbfd4347c
Aranud <aranud66@live.fr>
2023-02-24 17:06:48 +0100

    cgame: don't use editcomponent cmd to avoid help

--- src/cgame/cg_consolecmds.c
+++ src/cgame/cg_consolecmds.c
@@ -2901,279 +2901,277 @@ static void CG_EditHudComponentComplete(void)
 
 static void CG_CrosshairSize_f()
 {
-	if (trap_Argc() < 2)
+	if (trap_Argc() > 1)
 	{
-		trap_SendConsoleCommand(va("%s size\n", EDITCOMPONENT_CROSSHAIR_STRING));
-	}
-	else
-	{
-		trap_SendConsoleCommand(va("%s size %s %s position center\n", EDITCOMPONENT_CROSSHAIR_STRING, CG_Argv(1), CG_Argv(1)));
+		const char *token;
+
+		token = CG_Argv(1);
+
+		if (Q_isanumber(token))
+		{
+			float size = Q_atof(token);
+
+			CG_GetActiveHUD()->crosshair.location.x = (Ccg_WideX(SCREEN_WIDTH) - size) * .5f;
+			CG_GetActiveHUD()->crosshair.location.y = (SCREEN_HEIGHT - size) * .5f;
+			CG_GetActiveHUD()->crosshair.location.w = size;
+			CG_GetActiveHUD()->crosshair.location.h = size;
+		}
 	}
 }
 
 static void CG_CrosshairAlpha_f()
 {
-	if (trap_Argc() < 2)
+	if (trap_Argc() > 1)
 	{
-		trap_SendConsoleCommand(va("%s colorMain\n", EDITCOMPONENT_CROSSHAIR_STRING));
-	}
-	else
-	{
-		trap_SendConsoleCommand(va("%s colorMain %f %f %f %s\n", EDITCOMPONENT_CROSSHAIR_STRING,
-		                           CG_GetActiveHUD()->crosshair.colorMain[0],
-		                           CG_GetActiveHUD()->crosshair.colorMain[1],
-		                           CG_GetActiveHUD()->crosshair.colorMain[2],
-		                           CG_Argv(1)));
+		const char *token;
+
+		token = CG_Argv(1);
+
+		if (Q_isanumber(token))
+		{
+			CG_GetActiveHUD()->crosshair.colorMain[3] = Q_atof(token);
+		}
 	}
 }
 
 static void CG_CrosshairColor_f()
 {
-	if (trap_Argc() < 2)
+	if (trap_Argc() > 1)
 	{
-		trap_SendConsoleCommand(va("%s colorMain\n", EDITCOMPONENT_CROSSHAIR_STRING));
-	}
-	else if (trap_Argc() == 2)
-	{
-		trap_SendConsoleCommand(va("%s colorMain %s\n", EDITCOMPONENT_CROSSHAIR_STRING, CG_Argv(1)));
-	}
-	else
-	{
-		float r, g, b;
+		const char *colString;
+		vec4_t     color;
 
-		r = Q_atof(CG_Argv(1));
-		g = Q_atof(CG_Argv(2));
-		b = Q_atof(CG_Argv(3));
+		if (trap_Argc() == 2)
+		{
+			colString = CG_Argv(1);
+		}
+		else
+		{
+			colString = va("%s %s %s %f", CG_Argv(1), CG_Argv(2), CG_Argv(3), CG_GetActiveHUD()->crosshair.colorMain[3]);
+		}
 
-		trap_SendConsoleCommand(va("%s colorMain %f %f %f\n", EDITCOMPONENT_CROSSHAIR_STRING, r, g, b));
+		if (Q_ParseColor(colString, color))
+		{
+			CG_GetActiveHUD()->crosshair.colorMain[0] = color[0];
+			CG_GetActiveHUD()->crosshair.colorMain[1] = color[1];
+			CG_GetActiveHUD()->crosshair.colorMain[2] = color[2];
+		}
 	}
 }
 
 static void CG_CrosshairAlphaAlt_f()
 {
-	if (trap_Argc() < 2)
+	if (trap_Argc() > 1)
 	{
-		trap_SendConsoleCommand(va("%s colorSecondary\n", EDITCOMPONENT_CROSSHAIR_STRING));
-	}
-	else
-	{
-		trap_SendConsoleCommand(va("%s colorSecondary %f %f %f %s\n", EDITCOMPONENT_CROSSHAIR_STRING,
-		                           CG_GetActiveHUD()->crosshair.colorSecondary[0],
-		                           CG_GetActiveHUD()->crosshair.colorSecondary[1],
-		                           CG_GetActiveHUD()->crosshair.colorSecondary[2],
-		                           CG_Argv(1)));
+		const char *token;
+
+		token = CG_Argv(1);
+
+		if (Q_isanumber(token))
+		{
+			CG_GetActiveHUD()->crosshair.colorSecondary[3] = Q_atof(token);
+		}
 	}
 }
 
 static void CG_CrosshairColorAlt_f()
 {
-	if (trap_Argc() < 2)
+	if (trap_Argc() > 1)
 	{
-		trap_SendConsoleCommand(va("%s colorSecondary\n", EDITCOMPONENT_CROSSHAIR_STRING));
-	}
-	else if (trap_Argc() == 2)
-	{
-		trap_SendConsoleCommand(va("%s colorSecondary %s\n", EDITCOMPONENT_CROSSHAIR_STRING, CG_Argv(1)));
-	}
-	else
-	{
-		float r, g, b;
+		const char *colString;
+		vec4_t     color;
 
-		r = Q_atof(CG_Argv(1));
-		g = Q_atof(CG_Argv(2));
-		b = Q_atof(CG_Argv(3));
+		if (trap_Argc() == 2)
+		{
+			colString = CG_Argv(1);
+		}
+		else
+		{
+			colString = va("%s %s %s %f", CG_Argv(1), CG_Argv(2), CG_Argv(3), CG_GetActiveHUD()->crosshair.colorSecondary[3]);
+		}
 
-		trap_SendConsoleCommand(va("%s colorSecondary %f %f %f\n", EDITCOMPONENT_CROSSHAIR_STRING, r, g, b));
+		if (Q_ParseColor(colString, color))
+		{
+			CG_GetActiveHUD()->crosshair.colorSecondary[0] = color[0];
+			CG_GetActiveHUD()->crosshair.colorSecondary[1] = color[1];
+			CG_GetActiveHUD()->crosshair.colorSecondary[2] = color[2];
+		}
 	}
 }
 
 static void CG_CrosshairPulse_f()
 {
-	int style = CG_GetActiveHUD()->crosshair.style;
-
-	if (trap_Argc() < 2)
+	if (trap_Argc() > 1)
 	{
-		trap_SendConsoleCommand(va("%s style\n", EDITCOMPONENT_CROSSHAIR_STRING));
-		return;
+		if (Q_atoi(CG_Argv(1)))
+		{
+			ENABLEBIT(CG_GetActiveHUD()->crosshair.style, 0);
+		}
+		else
+		{
+			CLEARBIT(CG_GetActiveHUD()->crosshair.style, 0);
+		}
 	}
-
-	if (Q_atoi(CG_Argv(1)))
-	{
-		ENABLEBIT(style, 0);
-	}
-	else
-	{
-		CLEARBIT(style, 0);
-	}
-
-	trap_SendConsoleCommand(va("%s style %d\n", EDITCOMPONENT_CROSSHAIR_STRING, style));
 }
 
 static void CG_CrosshairHealth_f()
 {
-	int style = CG_GetActiveHUD()->crosshair.style;
-
-	if (trap_Argc() < 2)
+	if (trap_Argc() > 1)
 	{
-		trap_SendConsoleCommand(va("%s style\n", EDITCOMPONENT_CROSSHAIR_STRING));
-		return;
+		if (Q_atoi(CG_Argv(1)))
+		{
+			ENABLEBIT(CG_GetActiveHUD()->crosshair.style, 2);
+			ENABLEBIT(CG_GetActiveHUD()->crosshair.style, 3);
+		}
+		else
+		{
+			CLEARBIT(CG_GetActiveHUD()->crosshair.style, 2);
+			CLEARBIT(CG_GetActiveHUD()->crosshair.style, 3);
+		}
 	}
-
-	if (Q_atoi(CG_Argv(1)))
-	{
-		ENABLEBIT(style, 2);
-		ENABLEBIT(style, 3);
-	}
-	else
-	{
-		CLEARBIT(style, 2);
-		CLEARBIT(style, 3);
-	}
-
-	trap_SendConsoleCommand(va("%s style %d\n", EDITCOMPONENT_CROSSHAIR_STRING, style));
 }
 
 static consoleCommand_t commands[] =
 {
-	{ "testgun",              CG_TestGun_f              },
-	{ "testmodel",            CG_TestModel_f            },
-	{ "nextframe",            CG_TestModelNextFrame_f   },
-	{ "prevframe",            CG_TestModelPrevFrame_f   },
-	{ "nextskin",             CG_TestModelNextSkin_f    },
-	{ "prevskin",             CG_TestModelPrevSkin_f    },
-	{ "viewpos",              CG_Viewpos_f              },
-	{ "+scores",              CG_ScoresDown_f           },
-	{ "-scores",              CG_ScoresUp_f             },
-	{ "zoomin",               CG_ZoomIn_f               },
-	{ "zoomout",              CG_ZoomOut_f              },
-	{ "weaplastused",         CG_LastWeaponUsed_f       },
-	{ "weapnextinbank",       CG_NextWeaponInBank_f     },
-	{ "weapprevinbank",       CG_PrevWeaponInBank_f     },
-	{ "weapnext",             CG_NextWeapon_f           },
-	{ "weapprev",             CG_PrevWeapon_f           },
-	{ "weapalt",              CG_AltWeapon_f            },
-	{ "weapon",               CG_Weapon_f               },
-	{ "weaponbank",           CG_WeaponBank_f           },
-	{ "fade",                 CG_Fade_f                 },
+	{ "testgun",                CG_TestGun_f              },
+	{ "testmodel",              CG_TestModel_f            },
+	{ "nextframe",              CG_TestModelNextFrame_f   },
+	{ "prevframe",              CG_TestModelPrevFrame_f   },
+	{ "nextskin",               CG_TestModelNextSkin_f    },
+	{ "prevskin",               CG_TestModelPrevSkin_f    },
+	{ "viewpos",                CG_Viewpos_f              },
+	{ "+scores",                CG_ScoresDown_f           },
+	{ "-scores",                CG_ScoresUp_f             },
+	{ "zoomin",                 CG_ZoomIn_f               },
+	{ "zoomout",                CG_ZoomOut_f              },
+	{ "weaplastused",           CG_LastWeaponUsed_f       },
+	{ "weapnextinbank",         CG_NextWeaponInBank_f     },
+	{ "weapprevinbank",         CG_PrevWeaponInBank_f     },
+	{ "weapnext",               CG_NextWeapon_f           },
+	{ "weapprev",               CG_PrevWeapon_f           },
+	{ "weapalt",                CG_AltWeapon_f            },
+	{ "weapon",                 CG_Weapon_f               },
+	{ "weaponbank",             CG_WeaponBank_f           },
+	{ "fade",                   CG_Fade_f                 },
 
-	{ "mp_QuickMessage",      CG_QuickMessage_f         },
-	{ "mp_fireteammsg",       CG_QuickFireteams_f       },
-	{ "mp_fireteamadmin",     CG_QuickFireteamAdmin_f   },
-	{ "wm_sayPlayerClass",    CG_SayPlayerClass_f       },
-	{ "wm_ftsayPlayerClass",  CG_FTSayPlayerClass_f     },
+	{ "mp_QuickMessage",        CG_QuickMessage_f         },
+	{ "mp_fireteammsg",         CG_QuickFireteams_f       },
+	{ "mp_fireteamadmin",       CG_QuickFireteamAdmin_f   },
+	{ "wm_sayPlayerClass",      CG_SayPlayerClass_f       },
+	{ "wm_ftsayPlayerClass",    CG_FTSayPlayerClass_f     },
 
-	{ "spawnmenu",            CG_QuickSpawnpoint_f      },
+	{ "spawnmenu",              CG_QuickSpawnpoint_f      },
 
-	{ "VoiceChat",            CG_VoiceChat_f            },
-	{ "VoiceTeamChat",        CG_TeamVoiceChat_f        },
+	{ "VoiceChat",              CG_VoiceChat_f            },
+	{ "VoiceTeamChat",          CG_TeamVoiceChat_f        },
 
 	// say, teamsay, etc
-	{ "messageMode",          CG_MessageMode_f          },
-	{ "messageMode2",         CG_MessageMode_f          },
-	{ "messageMode3",         CG_MessageMode_f          },
-	{ "messageSend",          CG_MessageSend_f          },
+	{ "messageMode",            CG_MessageMode_f          },
+	{ "messageMode2",           CG_MessageMode_f          },
+	{ "messageMode3",           CG_MessageMode_f          },
+	{ "messageSend",            CG_MessageSend_f          },
 
-	{ "SetWeaponCrosshair",   CG_SetWeaponCrosshair_f   },
+	{ "SetWeaponCrosshair",     CG_SetWeaponCrosshair_f   },
 
-	{ "VoiceFireTeamChat",    CG_BuddyVoiceChat_f       },
+	{ "VoiceFireTeamChat",      CG_BuddyVoiceChat_f       },
 
-	{ "openlimbomenu",        CG_LimboMenu_f            },
+	{ "openlimbomenu",          CG_LimboMenu_f            },
 
-	{ "+stats",               CG_StatsDown_f            },
-	{ "-stats",               CG_StatsUp_f              },
-	{ "+topshots",            CG_topshotsDown_f         },
-	{ "-topshots",            CG_topshotsUp_f           },
-	{ "+objectives",          CG_objectivesDown_f       },
-	{ "-objectives",          CG_objectivesUp_f         },
+	{ "+stats",                 CG_StatsDown_f            },
+	{ "-stats",                 CG_StatsUp_f              },
+	{ "+topshots",              CG_topshotsDown_f         },
+	{ "-topshots",              CG_topshotsUp_f           },
+	{ "+objectives",            CG_objectivesDown_f       },
+	{ "-objectives",            CG_objectivesUp_f         },
 
-	{ "autoRecord",           CG_autoRecord_f           },
-	{ "toggleRecord",         CG_toggleRecord_f         },
-	{ "autoScreenshot",       CG_autoScreenShot_f       },
-	{ "currentTime",          CG_currentTime_f          },
-	{ "keyoff",               CG_keyOff_f               },
-	{ "keyon",                CG_keyOn_f                },
+	{ "autoRecord",             CG_autoRecord_f           },
+	{ "toggleRecord",           CG_toggleRecord_f         },
+	{ "autoScreenshot",         CG_autoScreenShot_f       },
+	{ "currentTime",            CG_currentTime_f          },
+	{ "keyoff",                 CG_keyOff_f               },
+	{ "keyon",                  CG_keyOn_f                },
 #ifdef FEATURE_MULTIVIEW
-	{ "mvactivate",           CG_mvToggleAll_f          },
-	{ "mvdel",                CG_mvDelete_f             },
-	{ "mvhide",               CG_mvHideView_f           },
-	{ "mvnew",                CG_mvNew_f                },
-	{ "mvshow",               CG_mvShowView_f           },
-	{ "mvswap",               CG_mvSwapViews_f          },
-	{ "mvtoggle",             CG_mvToggleView_f         },
-	{ "spechelp",             CG_toggleSpecHelp_f       },
+	{ "mvactivate",             CG_mvToggleAll_f          },
+	{ "mvdel",                  CG_mvDelete_f             },
+	{ "mvhide",                 CG_mvHideView_f           },
+	{ "mvnew",                  CG_mvNew_f                },
+	{ "mvshow",                 CG_mvShowView_f           },
+	{ "mvswap",                 CG_mvSwapViews_f          },
+	{ "mvtoggle",               CG_mvToggleView_f         },
+	{ "spechelp",               CG_toggleSpecHelp_f       },
 #endif
-	{ "statsdump",            CG_dumpStats_f            },
-	{ "+vstr",                CG_vstrDown_f             },
-	{ "-vstr",                CG_vstrUp_f               },
+	{ "statsdump",              CG_dumpStats_f            },
+	{ "+vstr",                  CG_vstrDown_f             },
+	{ "-vstr",                  CG_vstrUp_f               },
 
-	{ "selectbuddy",          CG_SelectBuddy_f          },
+	{ "selectbuddy",            CG_SelectBuddy_f          },
 
-	{ "MapZoomIn",            CG_AutomapZoomIn_f        },
-	{ "MapZoomOut",           CG_AutomapZoomOut_f       },
-	{ "+mapexpand",           CG_AutomapExpandDown_f    },
-	{ "-mapexpand",           CG_AutomapExpandUp_f      },
+	{ "MapZoomIn",              CG_AutomapZoomIn_f        },
+	{ "MapZoomOut",             CG_AutomapZoomOut_f       },
+	{ "+mapexpand",             CG_AutomapExpandDown_f    },
+	{ "-mapexpand",             CG_AutomapExpandUp_f      },
 
-	{ "generateTracemap",     CG_GenerateTracemap       },
+	{ "generateTracemap",       CG_GenerateTracemap       },
 
-	{ "ToggleAutoMap",        CG_ToggleAutomap_f        }, // toggle automap on/off
+	{ "ToggleAutoMap",          CG_ToggleAutomap_f        }, // toggle automap on/off
 
-	{ "editSpeakers",         CG_EditSpeakers_f         },
-	{ "dumpSpeaker",          CG_DumpSpeaker_f          },
-	{ "modifySpeaker",        CG_ModifySpeaker_f        },
-	{ "undoSpeaker",          CG_UndoSpeaker_f          },
-	{ "cpm",                  CG_CPM_f                  },
-	{ "forcetapout",          CG_ForceTapOut_f          },
-	{ "timerSet",             CG_TimerSet_f             },
-	{ "timerReset",           CG_TimerReset_f           },
-	{ "resetTimer",           CG_TimerReset_f           }, // keep ETPro compatibility
-	{ "class",                CG_Class_f                },
-	{ "classmenu",            CG_ClassMenu_f            },
-	{ "teammenu",             CG_TeamMenu_f             },
-	{ "readHuds",             CG_ReadHuds_f             },
-	{ "writeHuds",            CG_WriteHuds_f            },
-	{ "sharetimer",           CG_ShareTimer_f           },
-	{ "sharetimer_buddy",     CG_ShareTimer_f           },
+	{ "editSpeakers",           CG_EditSpeakers_f         },
+	{ "dumpSpeaker",            CG_DumpSpeaker_f          },
+	{ "modifySpeaker",          CG_ModifySpeaker_f        },
+	{ "undoSpeaker",            CG_UndoSpeaker_f          },
+	{ "cpm",                    CG_CPM_f                  },
+	{ "forcetapout",            CG_ForceTapOut_f          },
+	{ "timerSet",               CG_TimerSet_f             },
+	{ "timerReset",             CG_TimerReset_f           },
+	{ "resetTimer",             CG_TimerReset_f           }, // keep ETPro compatibility
+	{ "class",                  CG_Class_f                },
+	{ "classmenu",              CG_ClassMenu_f            },
+	{ "teammenu",               CG_TeamMenu_f             },
+	{ "readHuds",               CG_ReadHuds_f             },
+	{ "writeHuds",              CG_WriteHuds_f            },
+	{ "sharetimer",             CG_ShareTimer_f           },
+	{ "sharetimer_buddy",       CG_ShareTimer_f           },
 #ifdef FEATURE_EDV
-	{ "+freecam_turnleft",    CG_FreecamTurnLeftDown_f  },
-	{ "-freecam_turnleft",    CG_FreecamTurnLeftUp_f    },
-	{ "+freecam_turnright",   CG_FreecamTurnRightDown_f },
-	{ "-freecam_turnright",   CG_FreecamTurnRightUp_f   },
+	{ "+freecam_turnleft",      CG_FreecamTurnLeftDown_f  },
+	{ "-freecam_turnleft",      CG_FreecamTurnLeftUp_f    },
+	{ "+freecam_turnright",     CG_FreecamTurnRightDown_f },
+	{ "-freecam_turnright",     CG_FreecamTurnRightUp_f   },
 
-	{ "+freecam_turnup",      CG_FreecamTurnUpDown_f    },
-	{ "-freecam_turnup",      CG_FreecamTurnUpUp_f      },
-	{ "+freecam_turndown",    CG_FreecamTurnDownDown_f  },
-	{ "-freecam_turndown",    CG_FreecamTurnDownUp_f    },
+	{ "+freecam_turnup",        CG_FreecamTurnUpDown_f    },
+	{ "-freecam_turnup",        CG_FreecamTurnUpUp_f      },
+	{ "+freecam_turndown",      CG_FreecamTurnDownDown_f  },
+	{ "-freecam_turndown",      CG_FreecamTurnDownUp_f    },
 
-	{ "+freecam_rollleft",    CG_FreecamRollLeftDown_f  },
-	{ "-freecam_rollleft",    CG_FreecamRollLeftUp_f    },
-	{ "+freecam_rollright",   CG_FreecamRollRightDown_f },
-	{ "-freecam_rollright",   CG_FreecamRollRightUp_f   },
-	{ "freecam",              CG_Freecam_f              },
-	{ "freecamsetpos",        CG_FreecamSetPos_f        },
-	{ "freecamgetpos",        CG_FreecamGetPos_f        },
+	{ "+freecam_rollleft",      CG_FreecamRollLeftDown_f  },
+	{ "-freecam_rollleft",      CG_FreecamRollLeftUp_f    },
+	{ "+freecam_rollright",     CG_FreecamRollRightDown_f },
+	{ "-freecam_rollright",     CG_FreecamRollRightUp_f   },
+	{ "freecam",                CG_Freecam_f              },
+	{ "freecamsetpos",          CG_FreecamSetPos_f        },
+	{ "freecamgetpos",          CG_FreecamGetPos_f        },
 
-	{ "noclip",               CG_NoClip_f               },
+	{ "noclip",                 CG_NoClip_f               },
 #endif
 	// objective info list for mappers/scripters (and players? - we might extend it)
-	{ "oinfo",                CG_PrintObjectiveInfo_f   },
-	{ "resetmaxspeed",        CG_ResetMaxSpeed_f        },
-	{ "listspawnpt",          CG_ListSpawnPoints_f      },
+	{ "oinfo",                  CG_PrintObjectiveInfo_f   },
+	{ "resetmaxspeed",          CG_ResetMaxSpeed_f        },
+	{ "listspawnpt",            CG_ListSpawnPoints_f      },
 
-	{ "loc",                  CG_Location_f             },
-	{ "camera",               CG_Camera_f               },
-	{ "edithud",              CG_EditHud_f              },
-	{ "editcomponent",        CG_EditComponent_f        },
+	{ "loc",                    CG_Location_f             },
+	{ "camera",                 CG_Camera_f               },
+	{ "edithud",                CG_EditHud_f              },
+	{ "editcomponent",          CG_EditComponent_f        },
 
 	// TODO: Implement "alias" system and create those as customizable alias command
-	{ "cg_crosshairSize_f",     CG_CrosshairSize_f      },
-	{ "cg_crosshairAlpha_f",    CG_CrosshairAlpha_f     },
-	{ "cg_crosshairColor_f",    CG_CrosshairColor_f     },
-	{ "cg_crosshairAlphaAlt_f", CG_CrosshairAlphaAlt_f  },
-	{ "cg_crosshairColorAlt_f", CG_CrosshairColorAlt_f  },
-	{ "cg_crosshairPulse_f",    CG_CrosshairPulse_f     },
-	{ "cg_crosshairHealth_f",   CG_CrosshairHealth_f    },
+	{ "cg_crosshairSize_f",     CG_CrosshairSize_f        },
+	{ "cg_crosshairAlpha_f",    CG_CrosshairAlpha_f       },
+	{ "cg_crosshairColor_f",    CG_CrosshairColor_f       },
+	{ "cg_crosshairAlphaAlt_f", CG_CrosshairAlphaAlt_f    },
+	{ "cg_crosshairColorAlt_f", CG_CrosshairColorAlt_f    },
+	{ "cg_crosshairPulse_f",    CG_CrosshairPulse_f       },
+	{ "cg_crosshairHealth_f",   CG_CrosshairHealth_f      },
 
-	{ NULL,                   NULL                      }
+	{ NULL,                     NULL                      }
 };
 
 /**
