4c6537c2fae4a571bc58540aa263816cbef391aa
rafal1137 <rafal1137@yahoo.pl>
2023-07-18 00:57:11 +0200

    app: get basic gamepad joystick movement

--- app/src/main/java/org/etlegacy/app/ETLActivity.java
+++ app/src/main/java/org/etlegacy/app/ETLActivity.java
@@ -1,5 +1,7 @@
 package org.etlegacy.app;
 
+import static java.sql.Types.NULL;
+
 import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.Color;
@@ -9,6 +11,7 @@ import android.os.Handler;
 import android.os.Looper;
 import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.InputDevice;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
@@ -26,6 +29,7 @@ public class ETLActivity extends SDLActivity implements JoyStickListener {
 
     static volatile boolean UiMenu = false;
     ImageButton btn;
+	boolean is_relative = false;
 
     /**
      * Get an uiMenu boolean variable
@@ -319,13 +323,6 @@ public class ETLActivity extends SDLActivity implements JoyStickListener {
     }
 
     @Override
-    protected String[] getArguments() {
-        String[] etl_param = new String[15];
-        etl_param[0] = "vid_restart";
-        return etl_param;
-    }
-
-    @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
@@ -341,10 +338,14 @@ public class ETLActivity extends SDLActivity implements JoyStickListener {
 
         clipboardGetText();
 
-        if(Build.VERSION.SDK_INT > Build.VERSION_CODES.M)
-            setRelativeMouseEnabled(true);
-        else
-            setRelativeMouseEnabled(false);
+        if(Build.VERSION.SDK_INT > Build.VERSION_CODES.M) {
+			is_relative = true;
+			setRelativeMouseEnabled(true);
+		}
+        else {
+			is_relative = false;
+			setRelativeMouseEnabled(false);
+		}
 
         if (isAndroidTV() || isChromebook()) {
             Log.v("ETL", "AndroidTV / ChromeBook Detected, Display UI Disabled!");
@@ -414,7 +415,83 @@ public class ETLActivity extends SDLActivity implements JoyStickListener {
 
     }
 
-    @Override
+	private static float getCenteredAxis(MotionEvent event, InputDevice device, int axis, int historyPos) {
+		final InputDevice.MotionRange range = device.getMotionRange(axis, event.getSource());
+
+		// A joystick at rest does not always report an absolute position of
+		// (0,0). Use the getFlat() method to determine the range of values
+		// bounding the joystick axis center.
+		if (range != null) {
+			final float flat = range.getFlat();
+			final float value = historyPos < 0 ? event.getAxisValue(axis): event.getHistoricalAxisValue(axis, historyPos);
+
+			// Ignore axis values that are within the 'flat' region of the
+			// joystick axis center.
+			if (Math.abs(value) > flat) {
+				return value;
+			}
+		}
+		return 0;
+	}
+
+	private void processJoystickInput(MotionEvent event, int historyPos) {
+
+		InputDevice inputDevice = event.getDevice();
+
+		// Calculate the horizontal distance to move by
+		// using the input value from one of these physical controls:
+		// the left control stick, hat axis, or the right control stick.
+		float x = getCenteredAxis(event, inputDevice, MotionEvent.AXIS_X, historyPos);
+		if (x == 0) {
+			x = getCenteredAxis(event, inputDevice, MotionEvent.AXIS_HAT_X, historyPos);
+		}
+		if (x == 0) {
+			x = getCenteredAxis(event, inputDevice, MotionEvent.AXIS_Z, historyPos);
+		}
+
+		// Calculate the vertical distance to move by
+		// using the input value from one of these physical controls:
+		// the left control stick, hat switch, or the right control stick.
+		float y = getCenteredAxis(event, inputDevice, MotionEvent.AXIS_Y, historyPos);
+		if (y == 0) {
+			y = getCenteredAxis(event, inputDevice, MotionEvent.AXIS_HAT_Y, historyPos);
+		}
+		if (y == 0) {
+			y = getCenteredAxis(event, inputDevice, MotionEvent.AXIS_RZ, historyPos);
+		}
+
+		if (event.getAxisValue(MotionEvent.AXIS_RZ) > 0.0f)
+			SDLActivity.onNativeMouse(NULL, NULL, 0, y, is_relative);
+		else if ((event.getAxisValue(MotionEvent.AXIS_Z) > 0.0f))
+			SDLActivity.onNativeMouse(NULL, NULL, x, 0, is_relative);
+
+	}
+
+	@Override
+	public boolean onGenericMotionEvent(MotionEvent event) {
+
+		//SDLControllerManager.initialize();
+
+		// Check that the event came from a game controller
+		if ((event.getSource() & InputDevice.SOURCE_JOYSTICK) == InputDevice.SOURCE_JOYSTICK && event.getAction() == MotionEvent.ACTION_MOVE) {
+			// Process all historical movement samples in the batch
+			final int historySize = event.getHistorySize();
+			// Process the movements starting from the
+			// earliest historical position in the batch
+			for (int i = 0; i < historySize; i++) {
+				// Process the event at historical position i
+				//SDLControllerManager.handleJoystickMotionEvent(event);
+				processJoystickInput(event, i);
+			}
+			// Process the current movement sample in the batch (position -1)
+			processJoystickInput(event, -1);
+			return true;
+		}
+		return super.onGenericMotionEvent(event);
+	}
+
+
+	@Override
     public void onMove(JoyStick joyStick, double angle, double power, int direction) {
 
         if (isAndroidTV() || isChromebook()) {
