2b9012889c1a52c81faf5903328e18804367fe41
rafal1137 <rafal1137@yahoo.pl>
2023-08-18 00:10:25 +0200

    sdl: fix crashing

--- src/sdl/sdl_input.c
+++ src/sdl/sdl_input.c
@@ -1849,7 +1849,7 @@ void IN_Init(void)
 	in_nograb = Cvar_Get("in_nograb", "0", CVAR_ARCHIVE);
 
 	in_joystick          = Cvar_Get("in_joystick", "0", CVAR_ARCHIVE_ND | CVAR_LATCH);
-    Cvar_Set(in_joystickThreshold , "0.25");
+	in_joystickThreshold = Cvar_Get("in_joystickThreshold", "0.25", CVAR_ARCHIVE_ND);
 
 	SDL_StartTextInput();
 
