3890f62278bc3f2df067128414a49dab1736306d
Jacker <shogaiam@gmail.com>
2024-02-06 00:15:20 +0200

    lnx-aarch64 build machine docker script first draft

--- cmake/Toolchain-cross-aarch64-linux.cmake
+++ cmake/Toolchain-cross-aarch64-linux.cmake
@@ -3,16 +3,22 @@
 #-------------------------------------------------------------------------
 
 # the name of the target operating system
+set(CMAKE_VERBOSE_MAKEFILE ON)
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_VERSION 1)
 set(CMAKE_SYSTEM_PROCESSOR aarch64)
 set(CMAKE_SIZEOF_VOID_P 8)
 
-set(CMAKE_C_COMPILER "aarch64-linux-gnu-gcc")
-set(CMAKE_CXX_COMPILER "aarch64-linux-gnu-g++")
-
-# here is the target environment located
-set(CMAKE_FIND_ROOT_PATH  /usr/${COMPILER_PREFIX})
+SET(COMPILER_PREFIX aarch64-linux-gnu)
+SET (CMAKE_C_COMPILER ${COMPILER_PREFIX}-gcc CACHE STRING "Set the cross-compiler tool gcc" FORCE)
+SET (CMAKE_CXX_COMPILER ${COMPILER_PREFIX}-g++ CACHE STRING "Set the cross-compiler tool g++" FORCE)
+SET (CMAKE_LINKER ${COMPILER_PREFIX}-ld CACHE STRING "Set the cross-compiler tool LD" FORCE)
+SET (CMAKE_AR ${COMPILER_PREFIX}-ar CACHE STRING "Set the cross-compiler tool AR" FORCE)
+SET (CMAKE_NM {COMPILER_PREFIX}-nm CACHE STRING "Set the cross-compiler tool NM" FORCE)
+SET (CMAKE_OBJCOPY ${COMPILER_PREFIX}-objcopy CACHE STRING "Set the cross-compiler tool OBJCOPY" FORCE)
+SET (CMAKE_OBJDUMP ${COMPILER_PREFIX}-objdump CACHE STRING "Set the cross-compiler tool OBJDUMP" FORCE)
+SET (CMAKE_RANLIB ${COMPILER_PREFIX}-ranlib CACHE STRING "Set the cross-compiler tool RANLIB" FORCE)
+SET (CMAKE_STRIP {COMPILER_PREFIX}-strip CACHE STRING "Set the cross-compiler tool RANLIB" FORCE)
 
 # adjust the default behaviour of the FIND_XXX() commands:
 # search headers and libraries in the target environment, search
@@ -38,7 +44,7 @@ else()
 	set(CMAKE_CXX_FLAGS "-march=armv8-a")
 endif()
 
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -nodefaultlibs -fno-builtin -fno-stack-protector -nostdinc++ -mno-outline-atomics -lc")
+set(CMAKE_LIBRARY_ARCHITECTURE aarch64-linux-gnu)
 
 # cache flags
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS}" CACHE STRING "c flags")
---
+++ misc/docker/lnx-aarch64.Dockerfile
@@ -0,0 +1,19 @@
+FROM debian:11.8-slim
+LABEL version="1.0"
+LABEL maintainer="mail@etlegacy.com"
+LABEL description="Linux build machine for the arm64 (aarch64) linux releases"
+
+RUN cd /tmp && \
+    dpkg --add-architecture arm64 && \
+	apt update && apt install -y \
+    gcc-aarch64-linux-gnu g++-aarch64-linux-gnu git make cmake autoconf libtool zip patch nasm g++ \
+    libx11-dev:arm64 libgl1-mesa-dev:arm64 libxext-dev:arm64 xserver-xorg-dev:arm64 libwayland-dev:arm64 \
+    libasound2-dev:arm64 libpulse-dev:arm64 libxrandr-dev:arm64 libxcursor-dev:arm64 libxi-dev:arm64 \
+    libxss-dev:arm64 libxcb-xinput-dev:arm64 libwayland-egl1:arm64 libwayland-egl1-mesa:arm64 libwayland-cursor0:arm64 \
+    xkbcommon-dev:arm64
+
+RUN git clone --branch v1.11.1 --depth 1 https://github.com/ninja-build/ninja.git && \
+    cmake -B ninja/build -S ninja && \
+    cmake --build ninja/build && \
+	cmake --install ninja/build && rm -Rf ninja
+
