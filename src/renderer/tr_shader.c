25988a8706b76f861077d8d354d18f8f31198d8c
Aranud <aranud66@live.fr>
2024-02-24 18:01:54 +0100

    renderer: prevent evaluation order violation

--- src/renderer/tr_shader.c
+++ src/renderer/tr_shader.c
@@ -3876,7 +3876,8 @@ static void ScanAndLoadShaderFiles(void)
 	// free in reverse order, so the temp files are all dumped
 	for (i = numShaders - 1; i >= 0 ; i--)
 	{
-		Q_strncpyz(p++, "\n", sum + numShaders * 2 - (p - s_shaderText));
+		Q_strncpyz(p, "\n", sum + numShaders * 2 - (p - s_shaderText));
+		++p;
 		Q_strncpyz(p, buffers[i], sum + numShaders * 2 - (p - s_shaderText));
 		ri.FS_FreeFile(buffers[i]);
 		buffers[i] = p;
