be3ae73721e216c7f5bad2e6d8b3252e9c532306
RaFaL <rafal1137@yahoo.pl>
2024-05-09 15:04:16 +0200

    easybuild: windows enable rend2 by default add option to set to enable SSE

--- easybuild.bat
+++ easybuild.bat
@@ -21,7 +21,8 @@ SET build_64=0
 SET mod_only=0
 SET use_autoupdate=1
 SET use_extra=1
-SET build_r2=0
+SET build_r2=1
+SET build_sse=0
 SET build_ssl=1
 SET build_auth=1
 SET wolf_ssl=0
@@ -90,6 +91,8 @@ IF NOT "%1"=="" (
 		SET build_type=Debug
 	) ELSE IF /I "%1"=="-nor2" (
 		SET build_r2=0
+	) ELSE IF /I "%1"=="-sse" (
+		SET build_sse=1
 	) ELSE IF /I "%1"=="-no-r2" (
 		SET build_r2=0
 	) ELSE IF /I "%1"=="-generator" (
@@ -387,7 +390,8 @@ GOTO :EOF
 	-DBUNDLED_WOLFSSL=!wolf_ssl! ^
 	-DBUNDLED_OPENSSL=!open_ssl! ^
 	-DFEATURE_SSL=!feature_ssl! ^
-	-DFEATURE_AUTH=!feature_auth!
+	-DFEATURE_AUTH=!feature_auth! ^
+	-ENABLE_SSE=!build_sse!
 
 	IF !mod_only!==1 (
 		SET local_build_string=!local_build_string! ^
