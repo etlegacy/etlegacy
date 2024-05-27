ed418eb92709718cef74ca2732bb690486cdf01a
Vorschreibung <vorschreibung@gmail.com>
2024-05-27 03:13:34 +0200

    misc: Appease ShellCheck for pot_generator.sh
    - Quote paths
    - Split overlong 'xgettext' commands
    - Transition brittle for-find with find-exec

--- misc/pot_generator.sh
+++ misc/pot_generator.sh
@@ -10,15 +10,21 @@ MOD_POT=etmain/locale/etlegacy_mod.pot
 VERSION=$(git describe --abbrev=0 --tags)
 
 # CLIENT
-touch ${CLIENT_POT}
+touch "${CLIENT_POT}"
 find src/server/ src/client/ src/qcommon/ -name '*.c' -o -name '*.cpp' | sort | xgettext \
-\--from-code=UTF-8 --package-name="ET: Legacy" --copyright-holder="ET: Legacy Team" --package-version="${VERSION}" --msgid-bugs-address="mail@etlegacy.com" -o ${CLIENT_POT} -k_ -k__ -kCL_TranslateString -f -
+    --from-code=UTF-8 \
+    --package-name="ET: Legacy" --package-version="${VERSION}" \
+    --copyright-holder="ET: Legacy Team" --msgid-bugs-address="mail@etlegacy.com" \
+    -o "${CLIENT_POT}" -k_ -k__ -kCL_TranslateString -f -
 
 # MOD
-touch ${MOD_POT}
+touch "${MOD_POT}"
 find src/game src/ui src/cgame etmain/ui -name '*.c' -o -name '*.cpp' -o -name '*.menu' | sort | xgettext \
-\--language=C --from-code=UTF-8 --package-name="ET: Legacy" --copyright-holder="ET: Legacy Team" --package-version="${VERSION}" --msgid-bugs-address="mail@etlegacy.com" -o ${MOD_POT} -k_ -k__ -kCG_TranslateString -ktrap_TranslateString -kTRANSLATE -f -
+    --language=C --from-code=UTF-8 \
+    --package-name="ET: Legacy" --package-version="${VERSION}" \
+    --copyright-holder="ET: Legacy Team" --msgid-bugs-address="mail@etlegacy.com" \
+    -o "${MOD_POT}" -k_ -k__ -kCG_TranslateString -ktrap_TranslateString -kTRANSLATE -f -
 
 # Update po files
-for PO in $(find etmain/locale/mod -type f -name "*.po"); do msgmerge -vU ${PO} ${MOD_POT}; done
-for PO in $(find etmain/locale/client -type f -name "*.po"); do msgmerge -vU ${PO} ${CLIENT_POT}; done
+find etmain/locale/mod    -type f -name "*.po" -exec msgmerge -vU {} "${MOD_POT}"    \;
+find etmain/locale/client -type f -name "*.po" -exec msgmerge -vU {} "${CLIENT_POT}" \;
