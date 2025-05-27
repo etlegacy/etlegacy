afe53b87577d6d98d8bbf7ee79eee7e3118d8364
Vorschreibung <vorschreibung@gmail.com>
2025-05-27 16:57:05 +0200

    misc: Add generated LuaLS definitions
    LuaLS definitions can be regenerated with a new added script in
    './misc/regenerate-luals-definitions.py' - which can be run cross
    platform via `pixi run regenerate-luals-definitions`.
    
    These definitions are automatically generated by parsing doxygen
    comments in './src/game/g_lua.c'.
    
    These have been ported from 'etlegacy-lua-docs' with some fixups.
    
    They can be used (for now) by putting a `.luarc.json` adjacent to your
    lua files with contents alike:
    
    	{
    		"workspace": {
    			"library": ["/home/youruser/workspace/etlegacy/misc/luals-definitions/"],
    			"checkThirdParty": false
    		},
    		"diagnostics.globals": [
    		]
    	}
    
    Most features work out of the box, HOOKS in your lua files however
    profit from being explicitly annotated with `---@type` with the same
    name of the hook-function to check parameter arity and return types.
    
    e.g. in whatever lua script you are working on:
    
    	---@type et_RunFrame
    	function et_RunFrame(levelTime)
    		-- ...
    	end

---
+++ misc/luals-definitions/manual.lua
@@ -0,0 +1,12 @@
+---@meta legacy
+
+---@class et
+et = {}
+
+---@alias Vec2 { number, number}
+
+---@alias Vec3 { number, number, number }
+
+---@alias Vec4 { number, number, number, number }
+
+---@alias Vec5 { number, number, number, number, number }
