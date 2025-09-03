5ee9f2784f8fe5df06ac2999c2e168bbaf5fbad2
Vorschreibung <vorschreibung@gmail.com>
2025-09-03 20:12:42 +0200

    client: Move CVARS to `src/client/cl_cvars.{c,h}`

--- src/client/client.h
+++ src/client/client.h
@@ -52,6 +52,8 @@
 #include <jni.h>
 #endif
 
+#include "cl_cvars.h"
+
 #define RETRANSMIT_TIMEOUT  3000    ///< time between connection packet retransmits
 
 #define LIMBOCHAT_WIDTH     140     ///< NOTE: buffer size indicator, not related to screen bbox
@@ -447,82 +449,6 @@ extern clientStatic_t cls;
 extern vm_t        *cgvm;       ///< interface to cgame dll or vm
 extern vm_t        *uivm;       ///< interface to ui dll or vm
 extern refexport_t re;          ///< interface to refresh .dll
-
-// cvars
-
-extern cvar_t *cl_nodelta;
-extern cvar_t *cl_debugMove;
-extern cvar_t *cl_noprint;
-extern cvar_t *cl_timegraph;
-extern cvar_t *cl_maxpackets;
-extern cvar_t *cl_packetdup;
-extern cvar_t *cl_shownet;
-extern cvar_t *cl_shownuments;
-extern cvar_t *cl_showSend;
-extern cvar_t *cl_showServerCommands;
-extern cvar_t *cl_timeNudge;
-extern cvar_t *cl_extrapolationMargin;
-extern cvar_t *cl_showTimeDelta;
-extern cvar_t *cl_freezeDemo;
-
-extern cvar_t *cl_yawspeed;
-extern cvar_t *cl_pitchspeed;
-extern cvar_t *cl_run;
-extern cvar_t *cl_anglespeedkey;
-
-extern cvar_t *cl_recoilPitch;
-
-extern cvar_t *cl_bypassMouseInput;
-
-extern cvar_t *cl_doubletapdelay;
-
-extern cvar_t *cl_sensitivity;
-extern cvar_t *cl_freelook;
-
-extern cvar_t *cl_mouseAccel;
-extern cvar_t *cl_showMouseRate;
-
-extern cvar_t *cl_avidemo;
-extern cvar_t *cl_aviMotionJpeg;
-extern cvar_t *cl_aviFrameRate;
-extern cvar_t *cl_aviPipeFormat;
-extern cvar_t *cl_aviPipeExtension;
-
-extern cvar_t *m_pitch;
-extern cvar_t *m_yaw;
-extern cvar_t *m_forward;
-extern cvar_t *m_side;
-extern cvar_t *m_filter;
-
-extern cvar_t *j_pitch;
-extern cvar_t *j_yaw;
-extern cvar_t *j_forward;
-extern cvar_t *j_side;
-extern cvar_t *j_up;
-extern cvar_t *j_pitch_axis;
-extern cvar_t *j_yaw_axis;
-extern cvar_t *j_forward_axis;
-extern cvar_t *j_side_axis;
-extern cvar_t *j_up_axis;
-
-extern cvar_t *cl_timedemo;
-
-extern cvar_t *cl_activeAction;
-extern cvar_t *cl_autorecord;
-
-extern cvar_t *cl_allowDownload;
-extern cvar_t *cl_conXOffset;
-
-extern cvar_t *cl_missionStats;
-
-extern cvar_t *cl_profile;
-extern cvar_t *cl_defaultProfile;
-
-extern cvar_t *cl_consoleKeys;
-
-extern cvar_t *cl_interpolation;
-
-extern cvar_t *con_scale;
 
 //=================================================
 
