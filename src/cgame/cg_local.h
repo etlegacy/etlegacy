c976e410baf53e9821baf2f6023328b3440fd8bb
ryzyk-krzysiek <Frantisek.brv@gmail.com>
2025-11-17 15:05:25 +0100

    cgame: draw demo overlay/help window during intermission

--- src/cgame/cg_local.h
+++ src/cgame/cg_local.h
@@ -3264,6 +3264,7 @@ void CG_ShowHelp_On(int *status);
 #ifdef FEATURE_MULTIVIEW
 qboolean CG_ViewingDraw(void);
 #endif
+void CG_DrawDemoOverlay(void);
 
 // cg_scoreboard.c
 qboolean CG_DrawScoreboard(void);
