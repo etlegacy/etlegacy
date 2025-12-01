etlegacy/etlegacy

commit 7289d4b58bb89e5e319cfda826ae086a821cef94

cgame: Don't use blood when player is invulnerable
(Use smoke puffs instead)

src/cgame/cg_weapons.c
@@ -5339,9 +5339,11 @@ static void CG_AddFleshImpact(vec3_t end, vec3_t dir, int fleshEntityNum)
 	vec3_t    tmpv, tmpv2;
 	int       i, headshot;
 	centity_t *cent  = &cg_entities[fleshEntityNum];
-	qhandle_t shader = cg_blood.integer ? cgs.media.fleshSmokePuffShader : cgs.media.smokePuffShader;
+	qhandle_t shader = (cent->currentState.powerups & (1 << PW_INVULNERABLE))
+	    ? cgs.media.smokePuffShader
+	    : cgs.media.fleshSmokePuffShader;
 
-	if (ISVALIDCLIENTNUM(fleshEntityNum))
+	if (ISVALIDCLIENTNUM(fleshEntityNum) && !(cent->currentState.powerups & (1 << PW_INVULNERABLE)))
 	{
 		CG_Bleed(end, fleshEntityNum);
 	}
@@ -5404,7 +5406,10 @@ static void CG_AddFleshImpact(vec3_t end, vec3_t dir, int fleshEntityNum)
 		}
 	}
 
-	CG_AddBloodSplat(origin, end, dir);
+	if (!(cent->currentState.powerups & (1 << PW_INVULNERABLE)))
+	{
+		CG_AddBloodSplat(origin, end, dir);
+	}
 }
 
 /**

commit 609312af770d11061a892c60be11dbad0b9f29a8

cgame: fix blood puff doesn't turn off properly
regression from 7289d4b

src/cgame/cg_weapons.c
@@ -5339,7 +5339,7 @@ static void CG_AddFleshImpact(vec3_t end, vec3_t dir, int fleshEntityNum)
 	vec3_t    tmpv, tmpv2;
 	int       i, headshot;
 	centity_t *cent  = &cg_entities[fleshEntityNum];
-	qhandle_t shader = (cent->currentState.powerups & (1 << PW_INVULNERABLE))
+	qhandle_t shader = !cg_blood.integer || (cent->currentState.powerups & (1 << PW_INVULNERABLE))
 	    ? cgs.media.smokePuffShader
 	    : cgs.media.fleshSmokePuffShader;
 