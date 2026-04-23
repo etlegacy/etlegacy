be0f73c77f8f8977e15edc1c66d2f6b5754436e6
ryzyk-krzysiek <Frantisek.brv@gmail.com>
2026-04-23 18:12:12 +0200

    server: update 'sv_etltv_zoneMegs' cvar description

--- src/server/sv_init.c
+++ src/server/sv_init.c
@@ -1237,10 +1237,11 @@ void SV_Init(void)
 	sv_etltv_autoplay   = Cvar_Get("sv_etltv_autoplay", "0", CVAR_ARCHIVE_ND);
 	sv_etltv_clientname = Cvar_GetAndDescribe("sv_etltv_clientname", "ETLTV", CVAR_ARCHIVE_ND, "Name of the ETLTV client.");
 	sv_etltv_delay      = Cvar_GetAndDescribe("sv_etltv_delay", "0", CVAR_INIT, "Delay feed by number of seconds.");
-	sv_etltv_zoneMegs   = Cvar_GetAndDescribe("sv_etltv_zoneMegs", DEF_TVZONEMEGS_S, CVAR_INIT, "TV server queue memory zone. (in MB)");
-	sv_etltv_shownet    = Cvar_Get("sv_etltv_shownet", "0", CVAR_ARCHIVE_ND);
-	sv_etltv_queue_ms   = Cvar_Get("ettv_queue_ms", "-1", CVAR_ROM);    // ettv_queue_ms for ettv backward compatibility
-	sv_etltv_netblast   = Cvar_GetAndDescribe("sv_etltv_netblast", "1", CVAR_ARCHIVE_ND, "Send all message fragments at once.");
+	sv_etltv_zoneMegs   = Cvar_GetAndDescribe("sv_etltv_zoneMegs", DEF_TVZONEMEGS_S, CVAR_INIT, "Amount of memory (in MB) allocated for tv server when 'sv_etltv_delay' cvar is set.\n"
+	                                                                                            "This zone is used to store each incoming packet from master server for 'sv_etltv_delay' seconds.");
+	sv_etltv_shownet  = Cvar_Get("sv_etltv_shownet", "0", CVAR_ARCHIVE_ND);
+	sv_etltv_queue_ms = Cvar_Get("ettv_queue_ms", "-1", CVAR_ROM);      // ettv_queue_ms for ettv backward compatibility
+	sv_etltv_netblast = Cvar_GetAndDescribe("sv_etltv_netblast", "1", CVAR_ARCHIVE_ND, "Send all message fragments at once.");
 
 #if defined(FEATURE_IRC_SERVER) && defined(DEDICATED)
 	IRC_Init();
