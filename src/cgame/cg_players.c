1014f10affa6cc39e4f83b633d158e5e25a3ebdf
ryzyk-krzysiek <Frantisek.brv@gmail.com>
2026-07-14 23:34:39 +0200

    cgame: fix backward compatibility

--- src/cgame/cg_players.c
+++ src/cgame/cg_players.c
@@ -275,6 +275,8 @@ void CG_NewClientInfo(int clientNum)
 	newInfo.ammoclip      = ci->ammoclip;
 	newInfo.powerups      = ci->powerups;
 	newInfo.currentWeapon = ci->currentWeapon;
+	newInfo.spawnpt       = ci->spawnpt;
+	newInfo.mspawnpt      = ci->mspawnpt;
 
 	// isolate the player's name
 	v = Info_ValueForKey(configstring, "n");
@@ -298,13 +300,14 @@ void CG_NewClientInfo(int clientNum)
 	v                  = Info_ValueForKey(configstring, "lc");
 	newInfo.latchedcls = Q_atoi(v);
 
-	// backward compatibility
-	v               = Info_ValueForKey(configstring, "sp");
-	newInfo.spawnpt = Q_atoi(v);
+	if (CG_IsDemoVersionBelow(2, 85, 0))
+	{
+		v               = Info_ValueForKey(configstring, "sp");
+		newInfo.spawnpt = Q_atoi(v);
 
-	// backward compatibility
-	v                = Info_ValueForKey(configstring, "msp");
-	newInfo.mspawnpt = Q_atoi(v);
+		v                = Info_ValueForKey(configstring, "msp");
+		newInfo.mspawnpt = Q_atoi(v);
+	}
 
 	// rank
 	v            = Info_ValueForKey(configstring, "r");
@@ -574,10 +577,12 @@ void CG_NewClientInfo(int clientNum)
 		CG_ToggleShoutcasterMode(newInfo.shoutcaster);
 	}
 
-	// backward compatibility
-	if (newInfo.spawnpt != ci->spawnpt || newInfo.mspawnpt != ci->mspawnpt)
+	if (CG_IsDemoVersionBelow(2, 85, 0))
 	{
-		newInfo.spawnChangedTime = cg.time;
+		if (newInfo.spawnpt != ci->spawnpt || newInfo.mspawnpt != ci->mspawnpt)
+		{
+			newInfo.spawnChangedTime = cg.time;
+		}
 	}
 
 	// passing the clientNum since that's all we need, and we
